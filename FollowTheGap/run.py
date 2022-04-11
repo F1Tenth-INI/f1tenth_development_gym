f636c23e1bc71b26193902a8a145f8eb85c9a079
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2022-04-11 16:25:42 +0200

    Ready for multiple experiments with data recording

--- FollowTheGap/run.py
+++ FollowTheGap/run.py
@@ -8,97 +8,113 @@ import json
 
 from ftg_planner import FollowTheGapPlanner
 
+from Recorder import Recorder
+
 from OpenGL.GL import *
 from numba import njit
 
 from pyglet.gl import GL_POINTS
 import pyglet
 
+from tqdm import trange
+
 from f110_gym.envs.dynamic_models import vehicle_dynamics_st, pid
+
+number_of_experiments = 10
+render = True
+
+def add_noise(x, noise_level=0.1):
+    return x+noise_level*np.random.uniform(-1.0, 1.0)
+
 
 def main():
     """
     main entry point
     """
-
-    with open('config_example_map.yaml') as file:
-        conf_dict = yaml.load(file, Loader=yaml.FullLoader)
-    conf = Namespace(**conf_dict)
-
-    # First car
-    planner = FollowTheGapPlanner()
-    planner.speed_fraction = 1.5
-    planner.plot_lidar_data = False
-    planner.draw_lidar_data = True
-    planner.lidar_visualization_color = (255, 0, 255)
-
-
-
-    def render_callback(env_renderer):
-        # custom extra drawing function
-
-        e = env_renderer
-
-        # update camera to follow car
-        x = e.cars[0].vertices[::2]
-        y = e.cars[0].vertices[1::2]
-        top, bottom, left, right = max(y), min(y), min(x), max(x)
-        e.score_label.x = left
-        e.score_label.y = top - 700
-        e.left = left - 800
-        e.right = right + 800
-        e.top = top + 800
-        e.bottom = bottom - 800
-
-        planner.render(env_renderer)
-
-    env = gym.make('f110_gym:f110-v0', map=conf.map_path,
-                   map_ext=conf.map_ext, num_agents=1)
-    env.add_render_callback(render_callback)
-
-    obs, step_reward, done, info = env.reset(
-        np.array([[conf.sx, conf.sy, conf.stheta]]))
-
-    car = env.sim.agents[0] 
-
-    env.render()
-
-    laptime = 0.0
-    start = time.time()
-
-    render_index = 0
-    while not done:
-
-
-        ranges = obs['scans'][0]
-
-        # print("scan_angles", car.scan_angles)
-        # print("side_distances", car.side_distances)
-        # print("Scans",  obs['scans'][0])
-        # print("obs", obs)
-        # print("Car state", car_state)
+for i in trange(number_of_experiments):
+        print()
+        with open('config_example_map.yaml') as file:
+            conf_dict = yaml.load(file, Loader=yaml.FullLoader)
+        conf = Namespace(**conf_dict)
 
         # First car
-        odom_1 = {
-            'pose_x': obs['poses_x'][0],
-            'pose_y': obs['poses_y'][0],
-            'pose_theta': obs['poses_theta'][0],
-            'linear_vel_x': obs['linear_vels_x'][0],
-            'linear_vel_y': obs['linear_vels_y'][0],
-            'angular_vel_z': obs['ang_vels_z'][0]
-        }
+        planner = FollowTheGapPlanner()
+        planner.speed_fraction = 1.5
+        planner.plot_lidar_data = False
+        planner.draw_lidar_data = True
+        planner.lidar_visualization_color = (255, 0, 255)
 
-        speed, steer =  planner.process_observation(ranges, odom_1)
-        accl, sv = pid(speed, steer, car.state[3], car.state[2], car.params['sv_max'], car.params['a_max'], car.params['v_max'], car.params['v_min'])
+        R = Recorder(controller_name='FollowTheGap')
 
-    
-        obs, step_reward, done, info = env.step(np.array([[ accl, sv]]))
+        def render_callback(env_renderer):
+            # custom extra drawing function
 
-        laptime += step_reward
-        env.render(mode='human')
-        render_index += 1
+            e = env_renderer
 
-    print('Sim elapsed time:', laptime, 'Real elapsed time:', time.time()-start)
+            # update camera to follow car
+            x = e.cars[0].vertices[::2]
+            y = e.cars[0].vertices[1::2]
+            top, bottom, left, right = max(y), min(y), min(x), max(x)
+            e.score_label.x = left
+            e.score_label.y = top - 700
+            e.left = left - 800
+            e.right = right + 800
+            e.top = top + 800
+            e.bottom = bottom - 800
+
+            planner.render(env_renderer)
+
+        env = gym.make('f110_gym:f110-v0', map=conf.map_path,
+                       map_ext=conf.map_ext, num_agents=1)
+
+        env.add_render_callback(render_callback)
+
+        obs, step_reward, done, info = env.reset(
+            np.array([[conf.sx, conf.sy, conf.stheta]]))
+
+        car = env.sim.agents[0]
+
+        env.render()
+
+        laptime = 0.0
+        start = time.time()
+
+        render_index = 0
+        while not done:
+
+
+            ranges = obs['scans'][0]
+
+            # print("scan_angles", car.scan_angles)
+            # print("side_distances", car.side_distances)
+            # print("Scans",  obs['scans'][0])
+            # print("obs", obs)
+            # print("Car state", car_state)
+
+            # First car
+            odom_1 = {
+                'pose_x': obs['poses_x'][0],
+                'pose_y': obs['poses_y'][0],
+                'pose_theta': obs['poses_theta'][0],
+                'linear_vel_x': obs['linear_vels_x'][0],
+                'linear_vel_y': obs['linear_vels_y'][0],
+                'angular_vel_z': obs['ang_vels_z'][0]
+            }
+
+            speed, steer =  planner.process_observation(ranges, odom_1)
+            # R.save_data(control_inputs=(speed, steer), odometry=odom_1, ranges=ranges)
+            accl, sv = pid(add_noise(speed), add_noise(steer), car.state[3], car.state[2], car.params['sv_max'], car.params['a_max'], car.params['v_max'], car.params['v_min'])
+
+
+            obs, step_reward, done, info = env.step(np.array([[ accl, sv]]))
+
+            laptime += step_reward
+
+            if render == True:
+                env.render(mode='human')
+                render_index += 1
+        print()
+        print('Sim elapsed time:', laptime, 'Real elapsed time:', time.time()-start)
 
 
 if __name__ == '__main__':
--- Recorder.py
+++ Recorder.py
@@ -3,23 +3,48 @@ This is the class saving data for F1TENTH
 """
 
 import os
-import datetime
+from datetime import datetime
+import csv
+import numpy as np
+
+try:
+    # Use gitpython to get a current revision number and use it in description of experimental data
+    from git import Repo
+except:
+    pass
 
 ranges_decimate = True  # If true, saves only every tenth LIDAR scan
 ranges_forward_only = True # Only LIDAR scans in forward direction are saved
 
-path_to_experiment_recordings =
+rounding_decimals = 3
+
+path_to_experiment_recordings = '../ExperimentRecordings/'
 
 class Recorder:
-    def __init__(self):
+    def __init__(self, controller_name=None, create_header=True):
         self.ranges_decimate = ranges_decimate
         self.ranges_forward_only = ranges_forward_only
+        self.path_to_experiment_recordings = path_to_experiment_recordings
 
+        if controller_name is None:
+            self.controller_name = ''
+        else:
+            self.controller_name = controller_name
+
+        self.rounding_decimals = 3
+
+        self.headers_already_saved = False
         self.keys_ranges = None
-
+        self.keys_odometry = None
+        self.keys_control_inputs = None
+        self.csv_filepath = None
         self.ranges_dict = None
         self.odometry_dict = None
         self.control_inputs_dict = None
+        self.dict_to_save = {}
+
+        if create_header:
+            self.create_csv_header()
 
     def get_ranges(self, ranges):
 
@@ -28,41 +53,113 @@ class Recorder:
             ranges_to_save = ranges_to_save[200:880]
 
         if self.ranges_decimate:
-            ranges_to_save = ranges_to_save[:, :, 10]
+            ranges_to_save = ranges_to_save[::10]
 
         if self.keys_ranges is None:
             #Initialise
             self.keys_ranges = ['LIDAR_'+str(i) for i in range(len(ranges_to_save))]
-        else:
-            pass
 
         self.ranges_dict = dict(zip(self.keys_ranges, ranges_to_save))
 
     def get_odometry(self, odometry_dict):
         self.odometry_dict = odometry_dict
+        if self.keys_odometry is None:
+            self.keys_odometry = self.odometry_dict.keys()
 
     def get_control_inputs(self, control_inputs):
         speed, steering = control_inputs
         self.control_inputs_dict = {'speed': speed, 'steering': steering}
+        if self.keys_control_inputs is None:
+            self.keys_control_inputs = self.control_inputs_dict.keys()
 
-    def create_csv_path(self, csv_name = None):
+    def save_data(self, control_inputs, odometry, ranges):
+        self.get_control_inputs(control_inputs=control_inputs)
+        self.get_odometry(odometry_dict=odometry)
+        self.get_ranges(ranges=ranges)
+        self.save_csv()
+
+    def save_csv(self):
+
+        self.dict_to_save.update(self.control_inputs_dict)
+        self.dict_to_save.update(self.odometry_dict)
+        self.dict_to_save.update(self.ranges_dict)
+
+        # Save this dict
+        with open(self.csv_filepath, "a") as outfile:
+            writer = csv.writer(outfile)
+
+            if not self.headers_already_saved:
+                writer.writerow(self.dict_to_save.keys())
+                self.headers_already_saved = True
+
+            if self.rounding_decimals == np.inf:
+                pass
+            else:
+                self.dict_to_save = {key: np.around(value, self.rounding_decimals)
+                                     for key, value in self.dict_to_save.items()}
+
+            writer.writerow([float(x) for x in self.dict_to_save.values()])
+
+    def create_csv_header(self, csv_name=None):
 
         # Make folder to save data (if not yet existing)
         try:
-            os.makedirs(self.path_to_experiment_recordings)
+            os.makedirs(self.path_to_experiment_recordings[:-1])
         except FileExistsError:
             pass
 
         # Set path where to save the data
         if csv_name is None or csv_name == '':
-            self.csv_filepath = self.path_to_experiment_recordings + 'CP_' + self.controller_name + str(
+            self.csv_filepath = self.path_to_experiment_recordings + 'F1TENTH_' + self.controller_name + '_' + str(
                 datetime.now().strftime('_%Y-%m-%d_%H-%M-%S')) + '.csv'
         else:
             self.csv_filepath = csv_name
             if csv_name[-4:] != '.csv':
                 self.csv_filepath += '.csv'
 
+            # If such file exists, append index to the end (do not overwrite)
+            net_index = 1
+            logpath_new = self.csv_filepath
+            while True:
+                if os.path.isfile(logpath_new):
+                    logpath_new = self.csv_filepath[:-4]
+                else:
+                    self.csv_filepath = logpath_new
+                    break
+                logpath_new = logpath_new + '-' + str(net_index) + '.csv'
+                net_index += 1
+
+        # Write the header of .csv file
+        with open(self.csv_filepath, "a") as outfile:
+            writer = csv.writer(outfile)
+
+            writer.writerow(['# ' + 'This is F1TENTH simulation from {} at time {}'
+                            .format(datetime.now().strftime('%d.%m.%Y'), datetime.now().strftime('%H:%M:%S'))])
+            try:
+                repo = Repo('../')
+                git_revision = repo.head.object.hexsha
+            except:
+                git_revision = 'unknown'
+            writer.writerow(['# ' + 'Done with git-revision: {}'
+                            .format(git_revision)])
+
+            writer.writerow(['#'])
+
+            writer.writerow(['# Controller: {}'.format(self.controller_name)])
+
+            writer.writerow(['#'])
+
+            writer.writerow(['# Data:'])
+
     def reset(self):
+        self.headers_already_saved = False
         self.keys_ranges = None
+        self.keys_odometry = None
+        self.keys_control_inputs = None
+        self.csv_filepath = None
+        self.ranges_dict = None
+        self.odometry_dict = None
+        self.control_inputs_dict = None
+        self.dict_to_save = {}
 
 
