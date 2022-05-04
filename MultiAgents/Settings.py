be040d98bcaa28eced9c0b27cc39f02b8d4d6347
Florian Bolli <mail@florianbolli.ch>
2022-05-04 16:09:23 +0200

    Added Tobi's random obstacles and Settings

---
+++ MultiAgents/Settings.py
@@ -0,0 +1,11 @@
+# Configuration for main
+class Settings:
+    
+    # MAP_CONFIG_FILE =  "MultiAgents/config_example_map.yaml"
+    MAP_CONFIG_FILE =  "MultiAgents/config_Oschersleben.yaml"
+
+    PLACE_RANDOM_OBSTACLES = True
+    CAMERA_AUTO_FOLLOW = True
+    
+    RENDER_MODE = "human"
+    # RENDER_MODE = "human_fast"
