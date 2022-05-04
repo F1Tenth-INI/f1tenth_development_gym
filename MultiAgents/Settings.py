977f1c343856d0ca1c9d6bee222711410f077220
Florian Bolli <mail@florianbolli.ch>
2022-05-04 16:16:20 +0200

    Added Xiangs controller

--- MultiAgents/Settings.py
+++ MultiAgents/Settings.py
@@ -4,8 +4,8 @@ class Settings:
     # MAP_CONFIG_FILE =  "MultiAgents/config_example_map.yaml"
     MAP_CONFIG_FILE =  "MultiAgents/config_Oschersleben.yaml"
 
-    PLACE_RANDOM_OBSTACLES = True
-    CAMERA_AUTO_FOLLOW = True
+    PLACE_RANDOM_OBSTACLES = False
+    CAMERA_AUTO_FOLLOW = False
     
     RENDER_MODE = "human"
     # RENDER_MODE = "human_fast"
