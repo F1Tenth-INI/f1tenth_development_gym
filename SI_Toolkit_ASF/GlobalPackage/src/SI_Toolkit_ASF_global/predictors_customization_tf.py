e977ac270bf93cb92da711f4a19aae775a987f8b
Florian Bolli <mail@florianbolli.ch>
2022-06-02 16:00:36 +0200

    st_predictor in tensorflow

--- SI_Toolkit_ASF/GlobalPackage/src/SI_Toolkit_ASF_global/predictors_customization_tf.py
+++ SI_Toolkit_ASF/GlobalPackage/src/SI_Toolkit_ASF_global/predictors_customization_tf.py
@@ -18,7 +18,7 @@ class next_state_predictor_ODE_tf():
 
         if disable_individual_compilation:
             # self.step = self._step
-            self.step = self._step_ks
+            self.step = self._step_st
         else:
             self.step = Compile(self._step)
 
@@ -26,11 +26,14 @@ class next_state_predictor_ODE_tf():
     def _step(self, s, Q, params):
         '''
         Parallaley executes steps frim initial state s[i] with control input Q[i] for every i
-        @param s: (2000, 3) all initial states for every step 
-        @param s: (2000, 2) all control inputs for every step
+        @param s: (batch_size, len(state)) all initial states for every step 
+        @param s: (batch_size, len(control_input)) all control inputs for every step
         @param params: TODO: Parameters of the car
-        returns s_next: (2000, 3) all nexts states 
+        returns s_next: (batch_size, len(state)) all nexts states 
         '''
+        
+        number_of_rollouts = tf.shape(s)[0]
+
 
         pose_x = s[:, 0]
         pose_y = s[:, 1]
@@ -46,29 +49,75 @@ class next_state_predictor_ODE_tf():
 
         # s_next = tf.stack([pose_x, pose_y, pose_theta], axis=1)
         
-        s_next = tf.stack([pose_x, pose_y, tf.zeros([2000]), tf.zeros([2000]), pose_theta, tf.zeros([2000]), tf.zeros([2000])], axis=1)
+        s_next = tf.stack([pose_x, pose_y, tf.zeros([number_of_rollouts]), tf.zeros([number_of_rollouts]), pose_theta, tf.zeros([number_of_rollouts]), tf.zeros([number_of_rollouts])], axis=1)
         
+
+        return s_next
+    
+    def _step_ks(self, s, Q, params):
+        '''
+        Parallaley executes steps frim initial state s[i] with control input Q[i] for every i
+        @param s: (batch_size, len(state)) all initial states for every step 
+        @param s: (batch_size, len(control_input)) all control inputs for every step
+        @param params: TODO: Parameters of the car
+        returns s_next: (batch_size, len(state)) all nexts states 
+        '''
+        lf = 0.15875
+        lr = 0.17145
+        lwb = lf + lr
+        
+        # Dimensions
+        number_of_rollouts = tf.shape(s)[0]
+        
+        s_x = s[:, 0]           # Pose X
+        s_y = s[:, 1]           # Pose Y
+        delta = s[:, 2]         # Fron Wheel steering angle
+        theta = s[:, 3]         # Speed
+        psi = s[:, 4]           # Yaw Angle
+
+        delta_dot = Q[:, 1]     # steering angle velocity of front wheels
+        theta_dot = Q[:, 0]     # longitudinal acceleration
+        
+        # Constaints
+        theta_dot = self.accl_constraints(theta, theta_dot)
+        delta_dot = self.steering_constraints(delta, delta_dot)
+   
+        # Euler stepping
+        for _ in tf.range(self.intermediate_steps):
+            s_x_dot = tf.multiply(theta,tf.cos(psi))
+            s_y_dot = tf.multiply(theta,tf.sin(psi))
+            # delta_dot = delta_dot
+            # theta_dot = theta_dot
+            psi_dot = tf.divide(theta, lwb)*tf.tan(delta)
+            
+            s_x = s_x + self.t_step * s_x_dot
+            s_y = s_y + self.t_step * s_y_dot
+            delta = delta + self.t_step * delta_dot
+            theta = theta + self.t_step * theta_dot
+            psi = psi + self.t_step * psi_dot
+
+        s_next = tf.stack([s_x, s_y, delta,theta, psi,tf.zeros([number_of_rollouts]),tf.zeros([number_of_rollouts])], axis=1)
 
         return s_next
     
     def _step_st(self, s, Q, params):
         '''
         Parallaley executes steps frim initial state s[i] with control input Q[i] for every i
-        @param s: (2000, 3) all initial states for every step 
-        @param s: (2000, 2) all control inputs for every step
+        @param s: (batch_size, len(state)) all initial states for every step 
+        @param s: (batch_size, len(control_input)) all control inputs for every step
         @param params: TODO: Parameters of the car
-        returns s_next: (2000, 3) all nexts states 
+        returns s_next: (batch_size, len(state)) all nexts states 
         '''
         
         # params
-        mu = 1.0489
-        C_Sf = 21.92/1.0489
-        C_Sr = 21.92/1.0489
-        lf = 0.3048*3.793293
-        lr = 0.3048*4.667707
-        h = 0.3048*2.01355
-        m = 4.4482216152605/0.3048*74.91452
-        I = 4.4482216152605*0.3048*1321.416
+        mu = 1.0489       # friction coefficient  [-]
+        C_Sf = 4.718      # cornering stiffness front [1/rad]
+        C_Sr = 5.4562     # cornering stiffness rear [1/rad]
+        lf = 0.15875      # distance from venter of gracity to front axle [m]
+        lr = 0.17145      # distance from venter of gracity to rear axle [m]
+        h = 0.074         # center of gravity height of toal mass [m]
+        m = 3.74          # Total Mass of car [kg]
+        I = 0.04712       # Moment of inertia for entire mass about z axis  [kgm^2]
         g = 9.81
 
         # State
@@ -96,44 +145,25 @@ class next_state_predictor_ODE_tf():
         speed_too_low_for_st_indices = tf.cast(speed_too_low_for_st_indices, tf.float32)
         speed_not_too_low_for_st_indices = tf.cast(speed_not_too_low_for_st_indices, tf.float32)
         
-        
-
-        # if abs(x[3]) < 0.1:
-        # # wheelbase
-        # lwb = lf + lr
-
-        # # system dynamics
-        # x_ks = x[0:5]
-        # f_ks = vehicle_dynamics_ks(x_ks, u, mu, C_Sf, C_Sr, lf, lr, h, m, I, s_min, s_max, sv_min, sv_max, v_switch, a_max, v_min, v_max)
-        # # f = np.hstack((f_ks, np.array([u[1]/lwb*np.tan(x[2])+x[3]/(lwb*np.cos(x[2])**2)*u[0],
-        # # 0])))
-        
-        # f = np.hstack((f_ks, np.array([0,0])))
+        # TODO: Use ks model for slow speed
         
         for _ in tf.range(self.intermediate_steps):
             s_x_dot = tf.multiply(theta,tf.cos(tf.add(psi, beta)))
             s_y_dot = tf.multiply(theta,tf.sin(tf.add(psi, beta)))
+            
             # delta_dot = delta_dot
             # theta_dot = theta_dot
-            psi_dot = psi_dot
-            psi_dot_dot = tf.zeros([2000])
-            #* tf.multiply(delta, (C_Sf * (tf.fill([2000], g*lr) + theta_dot * h))) 
+            # psi_dot = psi_dot 
             
-            # p = psi_dot.numpy()[:10]
-            # print("p", p)
-            #tf.zeros([2000]) #-mu*m/(theta*I*(lr+lf))*(lf**2*C_Sf*(g*lr-theta_dot*h))*theta #+ lr**2*C_Sr*(g*lf + theta_dot*h))*psi_dot+mu*m/(I*(lr+lf))*(lr*C_Sr*(g*lf + theta_dot*h) - lf*C_Sf*(g*lr - theta_dot*h))*beta+mu*m/(I*(lr+lf))*lf*C_Sf*(g*lr - theta_dot*h)*delta,
-            
-            # -mu*m/(x[3]*I*(lr+lf))*(lf**2*C_Sf*(g*lr-u[1]*h) + lr**2*C_Sr*(g*lf + u[1]*h))*x[5] \
-            #     +mu*m/(I*(lr+lf))*(lr*C_Sr*(g*lf + u[1]*h) - lf*C_Sf*(g*lr - u[1]*h))*x[6] \
-            #     +mu*m/(I*(lr+lf))*lf*C_Sf*(g*lr - u[1]*h)*x[2],
-            beta_dot = tf.zeros([2000]) 
-            #mu/(theta**2*(lr+lf))*(C_Sr*(g*lf + theta_dot*h)*lr)*theta #- C_Sf*(g*lr - theta_dot*h)*lf)-1)*psi_dot -mu/(theta*(lr+lf))*(C_Sr*(g*lf + theta_dot*h) + C_Sf*(g*lr-theta_dot*h))*beta +mu/(theta*(lr+lf))*(C_Sf*(g*lr-theta_dot*h))*delta
-                
-            #  (mu/(x[3]**2*(lr+lf))*(C_Sr*(g*lf + u[1]*h)*lr - C_Sf*(g*lr - u[1]*h)*lf)-1)*x[5] \
-            #     -mu/(x[3]*(lr+lf))*(C_Sr*(g*lf + u[1]*h) + C_Sf*(g*lr-u[1]*h))*x[6] \
-            #     +mu/(x[3]*(lr+lf))*(C_Sf*(g*lr-u[1]*h))*x[2]])
+            psi_dot_dot =  -mu*m/(theta*I*(lr+lf))*(lf**2*C_Sf*(g*lr-theta_dot*h) + lr**2*C_Sr*(g*lf + theta_dot*h))*psi_dot \
+                +mu*m/(I*(lr+lf))*(lr*C_Sr*(g*lf + theta_dot*h) - lf*C_Sf*(g*lr - theta_dot*h))*beta \
+                +mu*m/(I*(lr+lf))*lf*C_Sf*( g*lr - theta_dot*h)*delta
+
+            beta_dot = (mu/(theta**2*(lr+lf))*(C_Sr*(g*lf + theta_dot*h)*lr - C_Sf*(g*lr - theta_dot*h)*lf)-1)*psi_dot \
+                -mu/(theta*(lr+lf))*(C_Sr*(g*lf + theta_dot*h) + C_Sf*(g*lr-theta_dot*h))*beta \
+                +mu/(theta*(lr+lf))*(C_Sf*(g*lr-theta_dot*h))*delta
            
-            s_x = tf.add(s_x, tf.multiply(self.t_step, s_x_dot))
+            s_x = s_x + self.t_step * s_x_dot
             s_y = s_y + self.t_step * s_y_dot
             delta = delta + self.t_step * delta_dot
             theta = theta + self.t_step * theta_dot
@@ -183,7 +213,7 @@ class next_state_predictor_ODE_tf():
         velocity_not_too_high_indices = tf.cast(velocity_not_too_high_indices, tf.float32)
         
         pos_limit_velocity_too_high = tf.math.divide(a_max * v_switch, vel)
-        pos_limit_velocity_not_too_high = tf.tile(a_max, [2000])
+        pos_limit_velocity_not_too_high = a_max
         
         pos_limit = tf.multiply(velocity_too_high_indices, pos_limit_velocity_too_high) + tf.multiply(velocity_not_too_high_indices, pos_limit_velocity_not_too_high)
         
@@ -206,49 +236,7 @@ class next_state_predictor_ODE_tf():
         
             
          
-    def _step_ks(self, s, Q, params):
-        '''
-        Parallaley executes steps frim initial state s[i] with control input Q[i] for every i
-        @param s: (2000, 3) all initial states for every step 
-        @param s: (2000, 2) all control inputs for every step
-        @param params: TODO: Parameters of the car
-        returns s_next: (2000, 3) all nexts states 
-        '''
-        lf = 0.15875
-        lr = 0.17145
-        lwb = lf + lr
-        
-        
-        s_x = s[:, 0]           # Pose X
-        s_y = s[:, 1]           # Pose Y
-        delta = s[:, 2]         # Fron Wheel steering angle
-        theta = s[:, 3]         # Speed
-        psi = s[:, 4]           # Yaw Angle
-
-        delta_dot = Q[:, 1]     # steering angle velocity of front wheels
-        theta_dot = Q[:, 0]     # longitudinal acceleration
-        
-        # Constaints
-        theta_dot = self.accl_constraints(theta, theta_dot)
-        delta_dot = self.steering_constraints(delta, delta_dot)
-   
-        # Euler stepping
-        for _ in tf.range(self.intermediate_steps):
-            s_x_dot = tf.multiply(theta,tf.cos(psi))
-            s_y_dot = tf.multiply(theta,tf.sin(psi))
-            # delta_dot = delta_dot
-            # theta_dot = theta_dot
-            psi_dot = tf.divide(theta, lwb)*tf.tan(delta)
-            
-            s_x = s_x + self.t_step * s_x_dot
-            s_y = s_y + self.t_step * s_y_dot
-            delta = delta + self.t_step * delta_dot
-            theta = theta + self.t_step * theta_dot
-            psi = psi + self.t_step * psi_dot
-
-        s_next = tf.stack([s_x, s_y, delta,theta, psi,tf.zeros([2000]),tf.zeros([2000])], axis=1)
-
-        return s_next
+    
     
 
 class predictor_output_augmentation_tf:
