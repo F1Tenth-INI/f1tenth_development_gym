7b2e34d4d22ae086888ae153f16f5f1d3f62a2c0
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2022-06-03 11:53:18 +0200

    Adding user defined normalization correction

--- SI_Toolkit_ASF/GlobalPackage/src/SI_Toolkit_ASF_global/user_defined_normalization_correction.py
+++ SI_Toolkit_ASF/GlobalPackage/src/SI_Toolkit_ASF_global/user_defined_normalization_correction.py
@@ -1,6 +1,7 @@
 
 def apply_user_defined_normalization_correction(df_norm_info):
 
-    pass
+    df_norm_info.loc['min', 'linear_vel_x'] = -df_norm_info.loc['max', 'linear_vel_x']
+    df_norm_info.loc['min', 'speed'] = -df_norm_info.loc['max', 'speed']
 
     return df_norm_info
