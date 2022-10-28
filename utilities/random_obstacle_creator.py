69925e1c826741fb2ac27232985a203d6e1dd0f6
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2022-10-29 00:55:42 +0200

    Correcting paths for generation of random obstacles on the map

--- utilities/random_obstacle_creator.py
+++ utilities/random_obstacle_creator.py
@@ -13,8 +13,8 @@ from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
-obstacles_config_path = 'main/random_obstables.yaml'
-path_where_to_save_the_map = 'main/maps/WithRandomObstacles'
+obstacles_config_path = 'utilities/random_obstables.yaml'
+path_where_to_save_the_map = 'utilities/maps/WithRandomObstacles'
 
 def create_obstacles(map_arr, map_resolution, obstacles_config_path=obstacles_config_path, start_point=None):
 
