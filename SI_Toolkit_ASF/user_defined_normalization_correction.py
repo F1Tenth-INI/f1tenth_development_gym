c0b46ccd6a4ad3cd9d131ac73398d619a975c966
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2023-01-04 14:50:55 +0100

    DO NOT PUSH TO MAIN
    Quickfix by Marcin

--- SI_Toolkit_ASF/user_defined_normalization_correction.py
+++ SI_Toolkit_ASF/user_defined_normalization_correction.py
@@ -1,7 +1,8 @@
 
 def apply_user_defined_normalization_correction(df_norm_info):
-
-    df_norm_info.loc['min', 'linear_vel_x'] = -df_norm_info.loc['max', 'linear_vel_x']
-    df_norm_info.loc['min', 'translational_control'] = -df_norm_info.loc['max', 'translational_control']
-
+    try:
+        df_norm_info.loc['min', 'linear_vel_x'] = -df_norm_info.loc['max', 'linear_vel_x']
+        df_norm_info.loc['min', 'translational_control'] = -df_norm_info.loc['max', 'translational_control']
+    except KeyError:
+        pass
     return df_norm_info
