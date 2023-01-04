2da5e90487d40f68c92d7daa842e19a0ec991598
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2023-01-04 14:49:45 +0100

    waypoint coordinate transformation in waypoint utils
    USE_Waypoints = Relative/abolute/false
    
    distinction in Waypoints utilization introduced

--- utilities/Settings.py
+++ utilities/Settings.py
@@ -4,28 +4,29 @@ class Settings:
 
     CONTROLLER = 'mpc'  # Options: 'mpc', 'ftg' (follow the gap), neural (neural network),  Out of order: 'pp' (pure pursuit)
     ODE_MODEL_OF_CAR_DYNAMICS = 'ODE:st'  # Its the model that the predictor uses. Only used for mpc predictions, if ODE predictor chosen
-    # Options for ODE_MODEL_OF_CAR_DYNAMICS: 'ODE:simple', 'ODE:ks', 'ODE:st' # TODO: Currently only st discerns correctly between scenario with and without PID
+        # Options for ODE_MODEL_OF_CAR_DYNAMICS: 'ODE:simple', 'ODE:ks', 'ODE:st' # TODO: Currently only st discerns correctly between scenario with and without PID
 
 
     #Network to be used for Neural control in nni_planner   -> Path to model can be adapted in nni_planner (controller=neursl)
-    NET_NAME = 'GRU-68IN-64H1-64H2-2OUT-1'
-    ACCELERATION_TIME = 20       #nni 50, mpc 10
-    ACCELERATION_AMPLITUDE = 10        #nni 2, mpc 10
+    NET_NAME = 'Dense-44IN-64H1-64H2-2OUT-2'
+    USE_WAYPOINTS = 'relative'                  #CHECK either: False (ftg), 'relative' or 'absolute'
+    ACCELERATION_TIME = 10              #nni 50, mpc 10
+    ACCELERATION_AMPLITUDE = 10         #nni 2, mpc 10
 
 
     # Decide if to use PID as in the original F1TENTH implementation
     # Or bypass it.
     # Warning: Even if set to True, the PID algorithm is modified
     # with respect to F1TENTH implementation! Check gym/f110_gym/envs/dynamics_models.py for more details
-    WITH_PID = False
+    WITH_PID = False                    #CHECK
 
     DISABLE_AUTOMATIC_TIMEOUT = True
 
     QUAD_VIZ = True  # Visualization, only for Quadruped
 
-    NUMBER_OF_EXPERIMENTS = 1  # How many times to run the car racing experiment
+    NUMBER_OF_EXPERIMENTS = 3  # How many times to run the car racing experiment
     EXPERIMENTS_IN_SEPARATE_PROGRAMS = False
-    EXPERIMENT_LENGTH = 1200 #36000  # in timesteps, only valid if DISABLE_AUTOMATIC_TIMEOUT is True.
+    EXPERIMENT_LENGTH = 3000 #36000  # in timesteps, only valid if DISABLE_AUTOMATIC_TIMEOUT is True.
 
     TIMESTEP_CONTROL = 0.03    # Multiple of 0.01
     
@@ -46,17 +47,17 @@ class Settings:
     SAVE_RECORDINGS = True
 
     #Set Noise Level
-    NOISE_LEVEL_TRANSLATIONAL_CONTROL = 0.0 # ftg: 0.5  # mppi: 2.0
-    NOISE_LEVEL_ANGULAR_CONTROL = 0.0  # ftg: 0.05  # mppi: 3.0
+    NOISE_LEVEL_TRANSLATIONAL_CONTROL = 2.0 # ftg: 0.5  # mppi: 2.0
+    NOISE_LEVEL_ANGULAR_CONTROL = 2.0  # ftg: 0.05  # mppi: 3.0
 
     # Automatically follow the first car on the map
     CAMERA_AUTO_FOLLOW = True
     DRAW_POSITION_HISTORY = True
 
     # We can chose between slow rendering (human) and fast rendering (human_fast)
-    # RENDER_MODE = None
+    #RENDER_MODE = None
+    #RENDER_MODE = "human_fast"
     RENDER_MODE = "human"
-    # RENDER_MODE = "human"
     NUM_TRAJECTORIES_TO_PLOT = 20
 
     # If false the max range of LIDAR is considered, otherwise only forward cone
