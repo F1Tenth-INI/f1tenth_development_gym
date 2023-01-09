017c257acabf1da2ea27951922b111d90e82ab2e
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2023-01-09 09:11:32 +0100

    small adaptions for Testing Performance

--- utilities/Settings.py
+++ utilities/Settings.py
@@ -2,15 +2,15 @@ class Settings:
 
     ENVIRONMENT_NAME = 'Car'  # Car or Quadruped
 
-    CONTROLLER = 'mpc'  # Options: 'mpc', 'ftg' (follow the gap), neural (neural network),  Out of order: 'pp' (pure pursuit)
+    CONTROLLER = 'neural'  # Options: 'mpc', 'ftg' (follow the gap), neural (neural network),  Out of order: 'pp' (pure pursuit)
     ODE_MODEL_OF_CAR_DYNAMICS = 'ODE:st'  # Its the model that the predictor uses. Only used for mpc predictions, if ODE predictor chosen
         # Options for ODE_MODEL_OF_CAR_DYNAMICS: 'ODE:simple', 'ODE:ks', 'ODE:st' # TODO: Currently only st discerns correctly between scenario with and without PID
 
 
     #Network to be used for Neural control in nni_planner   -> Path to model can be adapted in nni_planner (controller=neursl)
-    NET_NAME = 'Dense-44IN-64H1-64H2-2OUT-3'
+    NET_NAME = 'Dense-44IN-64H1-64H2-2OUT-3_MPPI_wypts_oschers_speed7_noisydata'
     USE_WAYPOINTS = 'relative'                  #CHECK either: False (ftg), 'relative' or 'absolute'
-    ACCELERATION_TIME = 10              #nni 50, mpc 10
+    ACCELERATION_TIME = 30              #nni 50, mpc 10
     ACCELERATION_AMPLITUDE = 10         #nni 2, mpc 10
 
 
@@ -24,9 +24,9 @@ class Settings:
 
     QUAD_VIZ = True  # Visualization, only for Quadruped
 
-    NUMBER_OF_EXPERIMENTS = 300  # How many times to run the car racing experiment
+    NUMBER_OF_EXPERIMENTS = 1  # How many times to run the car racing experiment
     EXPERIMENTS_IN_SEPARATE_PROGRAMS = False
-    EXPERIMENT_LENGTH = 1000 #36000  # in timesteps, only valid if DISABLE_AUTOMATIC_TIMEOUT is True.
+    EXPERIMENT_LENGTH = 3000 #36000  # in timesteps, only valid if DISABLE_AUTOMATIC_TIMEOUT is True.
 
     TIMESTEP_CONTROL = 0.03    # Multiple of 0.01
     
@@ -47,13 +47,13 @@ class Settings:
     #MAP_CONFIG_FILE =  "utilities/maps_files/config_Hockenheim.yaml"           #Decrease_resolution_factor = 8
     #MAP_CONFIG_FILE =  "utilities/maps_files/config_IMS.yaml"                  #Decrease_resolution_factor = 12
     #MAP_CONFIG_FILE =  "utilities/maps_files/config_Melbourne.yaml"            #Decrease_resolution_factor = 6
-    #MAP_CONFIG_FILE =  "utilities/maps_files/config_MexicoCity.yaml"           #Decrease_resolution_factor = 8
+    #MAP_CONFIG_FILE =  "utilities/maps_files/config_MexicoCity.yaml"           #Decrease_resolution_factor = 9
     #MAP_CONFIG_FILE =  "utilities/maps_files/config_Montreal.yaml"              #Decrease_resolution_factor = 6   and  ignore_wypts = 2
     #MAP_CONFIG_FILE =  "utilities/maps_files/config_Monza.yaml"                #Decrease_resolution_factor = 6
     #MAP_CONFIG_FILE =  "utilities/maps_files/config_MoscowRaceway.yaml"        #Decrease_resolution_factor = 10
     MAP_CONFIG_FILE =  "utilities/maps_files/config_Oschersleben.yaml"         #Decrease_resolution_factor = 1
-    #MAP_CONFIG_FILE =  "utilities/maps_files/config_Sakhir.yaml"               #Decrease_resolution_factor = 10
-    #MAP_CONFIG_FILE =  "utilities/maps_files/config_SaoPaulo.yaml"             #Decrease_resolution_factor = 10
+    #MAP_CONFIG_FILE =  "utilities/maps_files/config_Sakhir.yaml"               #Decrease_resolution_factor = 11
+    #MAP_CONFIG_FILE =  "utilities/maps_files/config_SaoPaulo.yaml"             #Decrease_resolution_factor = 9 and ignore_wypts=2
     #MAP_CONFIG_FILE =  "utilities/maps_files/config_Sepang.yaml"               #Decrease_resolution_factor = 6
     #MAP_CONFIG_FILE =  "utilities/maps_files/config_Sochi.yaml"                #Decrease_resolution_factor = 10
 
@@ -76,9 +76,9 @@ class Settings:
     DRAW_POSITION_HISTORY = True
 
     # We can chose between slow rendering (human) and fast rendering (human_fast)
-    RENDER_MODE = None
+    #RENDER_MODE = None
     #RENDER_MODE = "human_fast"
-    #RENDER_MODE = "human"
+    RENDER_MODE = "human"
     NUM_TRAJECTORIES_TO_PLOT = 20
 
     # If false the max range of LIDAR is considered, otherwise only forward cone
