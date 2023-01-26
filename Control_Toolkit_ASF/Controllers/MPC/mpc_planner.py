d1211628e3f6f2079ec1cbf196bcfcb18689e84a
Florian Bolli <mail@florianbolli.ch>
2023-01-26 20:40:13 +0100

    fixes

--- Control_Toolkit_ASF/Controllers/MPC/mpc_planner.py
+++ Control_Toolkit_ASF/Controllers/MPC/mpc_planner.py
@@ -108,15 +108,17 @@ class mpc_planner:
 
         car_position = [s[POSE_X_IDX], s[POSE_Y_IDX]]
         self.waypoint_utils.update_next_waypoints(car_position)
+        
+        # print("next_waypoints", self.waypoint_utils.next_waypoints.shape)
 
         # Deprecated, meybe use for racing again?
         # Accelerate at the beginning (St model expoldes for small velocity)
         # Give it a little "Schupf"
-        # if self.simulation_index < 0:
-        #     self.simulation_index += 1
-        #     self.translational_control = 10
-        #     self.angular_control = 0
-        #     return self.translational_control, self.angular_control
+        if self.simulation_index < 4:
+            self.simulation_index += 1
+            self.translational_control = 10
+            self.angular_control = 0
+            return self.translational_control, self.angular_control
 
         if Settings.LOOK_FORWARD_ONLY:
             lidar_range_min = 200
