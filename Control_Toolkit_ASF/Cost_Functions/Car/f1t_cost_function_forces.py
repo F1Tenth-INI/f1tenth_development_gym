606481ce94d413e66d943f7d7a23eaf36f9a03f3
EnnioEvo <artemisvii@gmail.com>
2023-02-20 19:12:06 +0100

    f1t_cost_function_forces.py adapted to casadi

--- Control_Toolkit_ASF/Cost_Functions/Car/f1t_cost_function_forces.py
+++ Control_Toolkit_ASF/Cost_Functions/Car/f1t_cost_function_forces.py
@@ -9,13 +9,13 @@ from SI_Toolkit.computation_library import ComputationLibrary
 
 from utilities.state_utilities import *
 
-
 distance_normalization = 6.0
 
 # TODO: Config should be loaded at specific functions
 # load constants from config file config_controllers
 config = yaml.load(open(os.path.join("Control_Toolkit_ASF", "config_cost_function.yml"), "r"), Loader=yaml.FullLoader)
-config_controllers = yaml.load(open(os.path.join("Control_Toolkit_ASF", "config_controllers.yml"), "r"), Loader=yaml.FullLoader)
+config_controllers = yaml.load(open(os.path.join("Control_Toolkit_ASF", "config_controllers.yml"), "r"),
+                               Loader=yaml.FullLoader)
 
 mpc_type = config_controllers["mpc"]['optimizer']
 
@@ -40,8 +40,6 @@ class f1t_cost_function_forces(cost_function_base):
         super(f1t_cost_function_forces, self).__init__(controller, ComputationLib)
         self._P1 = None
         self._P2 = None
-
-
 
     # region updating P1 & P2
     @property
@@ -82,7 +80,7 @@ class f1t_cost_function_forces(cost_function_base):
     # TODO: Make it library agnostic. This also justifies why some methods are not static, although currently they could be
     def get_actuation_cost(self, u):
         cc_cost = R * (u ** 2)
-        return casadi.cumsum(cc_weight.numpy()*cc_cost,-1)
+        return casadi.cumsum(cc_weight.numpy() * cc_cost, -1)
 
     def get_terminal_speed_cost(self, terminal_state):
         ''' Compute penality for deviation from desired max speed'''
@@ -102,7 +100,7 @@ class f1t_cost_function_forces(cost_function_base):
             return tf.math.reduce_sum(ccrc_weight * ccrc, axis=-1)
             raise Exception('ccrc not implemented with nlp_forces')
         else:
-            return casadi.SX([[0.0],[0.0]])
+            return casadi.SX([[0.0], [0.0]])
 
     def get_acceleration_cost(self, u):
         ''' Calculate cost for deviation from desired acceleration at every timestep'''
@@ -122,15 +120,14 @@ class f1t_cost_function_forces(cost_function_base):
         return steering_cost
 
     def get_distance_to_waypoints_per_rollout(self, trajectory_points, waypoints):
-        trajectories_shape = tf.shape(trajectory_points)
+        trajectories_shape = trajectory_points.shape
         number_of_rollouts = trajectories_shape[0]
-        number_of_steps = trajectories_shape[1]
-        number_of_waypoints = tf.shape(waypoints)[0]
+        number_of_waypoints = waypoints.shape[0]
 
-        positions_of_trajectories = tf.reshape(trajectory_points, [trajectories_shape[0] * trajectories_shape[1], 2])
+        positions_of_trajectories = trajectory_points.reshape((trajectories_shape[0], 2))
         distance_to_waypoints = self.distances_from_list_to_list_of_points(positions_of_trajectories, waypoints)
-        distance_to_waypoints_per_rollout = tf.reshape(distance_to_waypoints,
-                                                       (number_of_rollouts, number_of_steps, number_of_waypoints))
+        distance_to_waypoints_per_rollout = casadi.reshape(distance_to_waypoints,
+                                                           (number_of_rollouts, number_of_waypoints))
         return distance_to_waypoints_per_rollout
 
     def get_distances_from_trajectory_points_to_closest_target_point(self, trajectory_points, waypoints):
@@ -138,10 +135,10 @@ class f1t_cost_function_forces(cost_function_base):
         min_distance_to_waypoints_per_rollout = tf.reduce_min(distance_to_waypoints_per_rollout, axis=2)
         return min_distance_to_waypoints_per_rollout
 
-    def get_nearest_waypoints_indices(self, trajectory_points, waypoints):
+    def get_nearest_waypoints_mask(self, trajectory_points, waypoints):
         distance_to_waypoints_per_rollout = self.get_distance_to_waypoints_per_rollout(trajectory_points, waypoints)
-        indices_nearest_waypoints = tf.argmin(distance_to_waypoints_per_rollout, axis=2)
-        return indices_nearest_waypoints
+        mask_nearest_waypoints = distance_to_waypoints_per_rollout == casadi.mmin(distance_to_waypoints_per_rollout)
+        return mask_nearest_waypoints
 
     def distances_to_list_of_points(self, point, points2):
         length = tf.shape(points2)[0]
@@ -171,7 +168,7 @@ class f1t_cost_function_forces(cost_function_base):
 
     def get_crash_cost(self, trajectories, border_points):
         return self.get_crash_cost_normed(trajectories,
-                                     border_points) * 1000000  # Disqualify trajectories too close to sensor points
+                                          border_points) * 1000000  # Disqualify trajectories too close to sensor points
 
     def get_target_distance_cost_normed(self, trajectories, target_points):
 
@@ -189,35 +186,39 @@ class f1t_cost_function_forces(cost_function_base):
         return target_distance_cost_weight * self.get_target_distance_cost_normed(trajectories, target_points)
 
     def get_distance_to_waypoints_cost(self, s, waypoint_positions):
-        car_positions = s[:, :, POSE_X_IDX:POSE_Y_IDX + 1]  # TODO: Maybe better access separatelly X&Y and concat them afterwards.
-        return self.get_distances_from_trajectory_points_to_closest_target_point(car_positions, waypoint_positions) * distance_to_waypoints_cost_weight
+        car_positions = s[:, :,
+                        POSE_X_IDX:POSE_Y_IDX + 1]  # TODO: Maybe better access separatelly X&Y and concat them afterwards.
+        return self.get_distances_from_trajectory_points_to_closest_target_point(car_positions,
+                                                                                 waypoint_positions) * distance_to_waypoints_cost_weight
 
     def distances_from_list_to_list_of_points(self, points1, points2):
 
-        length1 = tf.shape(points1)[0]
-        length2 = tf.shape(points2)[0]
+        length1 = points1.shape[0]
+        length2 = points2.shape[0]
 
-        points1 = tf.tile([points1], [1, 1, length2])
-        points1 = tf.reshape(points1, (length1 * length2, 2))
+        points1 = casadi.repmat(points1, (1, length2))
+        points1 = casadi.reshape(points1, (2, length1 * length2)).T
 
-        points2 = tf.tile([points2], [length1, 1, 1])
-        points2 = tf.reshape(points2, (length1 * length2, 2))
+        points2 = casadi.repmat(points2, (length1, 1))
+        points2 = casadi.reshape(points2, (length1 * length2, 2))  # doesn't change it
 
         diff = points2 - points1
-        squared_diff = tf.math.square(diff)
-        squared_dist = tf.reduce_sum(squared_diff, axis=1)
+        squared_diff = casadi.power(diff, 2)
+        squared_dist = casadi.cumsum(squared_diff, 1)[:, 1]
 
-        squared_dist = tf.reshape(squared_dist, [length1, length2])
+        # squared_dist = casadi.reshape(squared_dist, [length1, length2])
 
         return squared_dist
 
     def get_distance_to_wp_segments_cost(self, s, waypoints):
-        car_positions = s[:, :, POSE_X_IDX:POSE_Y_IDX + 1]  # TODO: Maybe better access separatelly X&Y and concat them afterwards.
-        waypoint_positions = waypoints[:,1:3]
+        car_positions = s[:,
+                        POSE_X_IDX:POSE_Y_IDX + 1]  # TODO: Maybe better access separatelly X&Y and concat them afterwards.
+        waypoint_positions = waypoints[:, 1:3]
 
         # Get nearest and the nearest_next waypoint for every position on the car's rollout
-        nearest_waypoint_indices = self.get_nearest_waypoints_indices(car_positions, waypoint_positions[:-1])
-        return self.get_squared_distances_to_nearest_wp_segment(car_positions, waypoint_positions, nearest_waypoint_indices)
+        nearest_waypoint_mask = self.get_nearest_waypoints_mask(car_positions, waypoint_positions[:-1, :])
+        return self.get_squared_distances_to_nearest_wp_segment(car_positions, waypoint_positions,
+                                                                nearest_waypoint_mask)
 
     def get_wp_segment_vectors(self, waypoint_positions, nearest_waypoint_indices):
         nearest_waypoints = tf.gather(waypoint_positions, nearest_waypoint_indices)
@@ -227,51 +228,45 @@ class f1t_cost_function_forces(cost_function_base):
         wp_segment_vectors = nearest_waypoints_next - nearest_waypoints
         return wp_segment_vectors
 
-    def get_squared_distances_to_nearest_wp_segment(self, car_positions, waypoint_positions, nearest_waypoint_indices):
-        nearest_waypoints = tf.gather(waypoint_positions, nearest_waypoint_indices)
-        nearest_waypoints_next = tf.gather(waypoint_positions, nearest_waypoint_indices + 1)
+    def get_squared_distances_to_nearest_wp_segment(self, car_positions, waypoint_positions, nearest_waypoint_mask):
+        # nearest_waypoints = tf.gather(waypoint_positions, nearest_waypoint_mask)
+        # nearest_waypoints_next = tf.gather(waypoint_positions, nearest_waypoint_mask + 1)
+        nearest_waypoints = casadi.hcat([nearest_waypoint_mask, casadi.SX.zeros(1, 1)]) @ waypoint_positions
+        nearest_waypoints_next =  casadi.hcat([casadi.SX.zeros(1, 1), nearest_waypoint_mask]) @ waypoint_positions
 
         # Get next waypoint segment vector for every position on the car's rollout
         wp_segment_vectors = nearest_waypoints_next - nearest_waypoints
-        wp_segment_vector_norms = tf.norm(wp_segment_vectors, axis=2)
-        
+        wp_segment_vector_norms = casadi.norm_2(wp_segment_vectors)
+
         # Get the vector from the next waypoint to the car position
-        vector_car_pos_to_nearest_wp = tf.subtract(car_positions,nearest_waypoints)
-        vector_car_pos_to_nearest_wp_norm = tf.norm(vector_car_pos_to_nearest_wp, axis=2)
-        vector_car_pos_to_nearest_wp_norm_square = tf.multiply(vector_car_pos_to_nearest_wp_norm, vector_car_pos_to_nearest_wp_norm)
-        
+        vector_car_pos_to_nearest_wp = car_positions - nearest_waypoints
+        vector_car_pos_to_nearest_wp_norm_square = vector_car_pos_to_nearest_wp @ vector_car_pos_to_nearest_wp.T
+
         # Get projection of car_pos_to_nearest_wp on nearest wp_segment
-        car_pos_to_nearest_wp_dot_wp_segment =  tf.reduce_sum(vector_car_pos_to_nearest_wp * wp_segment_vectors, axis=2)
+        car_pos_to_nearest_wp_dot_wp_segment = vector_car_pos_to_nearest_wp @ wp_segment_vectors.T
         projection_car_pos_to_wp_on_wp_segment = car_pos_to_nearest_wp_dot_wp_segment / wp_segment_vector_norms
-        projection_square = tf.multiply(projection_car_pos_to_wp_on_wp_segment, projection_car_pos_to_wp_on_wp_segment)
-        
+        projection_square = projection_car_pos_to_wp_on_wp_segment @ projection_car_pos_to_wp_on_wp_segment.T
+
         # Pytagoras
         distance_to_segment_square = vector_car_pos_to_nearest_wp_norm_square - projection_square
         distance_to_wp_segments_cost = distance_to_segment_square
         return distance_to_wp_segments_cost
-    
+
     def get_velocity_difference_to_wp_cost(self, s, waypoints):
         # Get nearest and the nearest_next waypoint for every position on the car's rollout
-        car_positions = s[:, :, POSE_X_IDX:POSE_Y_IDX + 1]  # TODO: Maybe better access separatelly X&Y and concat them afterwards.
+        car_positions = s[:,
+                        POSE_X_IDX:POSE_Y_IDX + 1]  # TODO: Maybe better access separatelly X&Y and concat them afterwards.
         waypoint_positions = waypoints[:, 1:3]
-        nearest_waypoint_indices = self.get_nearest_waypoints_indices(car_positions, waypoint_positions[:-1])
-        car_vel_x = s[:, :, LINEAR_VEL_X_IDX]
-        velocity_difference_to_wp = self.get_velocity_difference_to_wp(car_vel_x, waypoints, nearest_waypoint_indices)
-        horizon = tf.cast(tf.shape(s)[1], dtype=tf.float32)
-        velocity_difference_to_wp_normed = velocity_difference_to_wp / horizon
-        return velocity_diff_to_waypoints_cost_weight * velocity_difference_to_wp_normed
-        
-    def get_velocity_difference_to_wp(self, car_vel, waypoints, nearest_waypoint_indices):
+        nearest_waypoint_mask = self.get_nearest_waypoints_mask(car_positions, waypoint_positions[:-1, :])
+        car_vel_x = s[:, LINEAR_VEL_X_IDX]
+        velocity_difference_to_wp = self.get_velocity_difference_to_wp(car_vel_x, waypoints, nearest_waypoint_mask)
+        return velocity_diff_to_waypoints_cost_weight * velocity_difference_to_wp
 
-        nearest_waypoints = tf.gather(waypoints, nearest_waypoint_indices)
+    def get_velocity_difference_to_wp(self, car_vel, waypoints, nearest_waypoint_mask):
 
-        nearest_waypoint_vel_x = waypoint_velocity_factor * nearest_waypoints[:,:,5]
+        nearest_waypoints = casadi.hcat([nearest_waypoint_mask, casadi.SX.zeros(1, 1)]) @ waypoints
 
-        nearest_waypoint_vel_x = self.lib.clip(nearest_waypoint_vel_x, 0.5, 17.5)
-        
-        vel_difference = tf.abs(nearest_waypoint_vel_x - car_vel)
+        nearest_waypoint_vel_x = waypoint_velocity_factor * nearest_waypoints[ :, 5]
+
+        vel_difference = casadi.norm_1(nearest_waypoint_vel_x - car_vel)
         return vel_difference
-        
-        
-        
-        
