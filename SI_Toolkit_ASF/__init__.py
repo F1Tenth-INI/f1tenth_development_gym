5d76a5785ff2609fef3c01d1faa3d44f3c1d3d8f
Florian Bolli <mail@florianbolli.ch>
2023-03-02 19:40:37 +0100

    save configs into recordings

--- SI_Toolkit_ASF/__init__.py
+++ SI_Toolkit_ASF/__init__.py
@@ -1,2 +1,5 @@
-GLOBALLY_DISABLE_COMPILATION = False  # Set to False to use tf.function
+from utilities.Settings import Settings
+
+
+GLOBALLY_DISABLE_COMPILATION = Settings.GLOBALLY_DISABLE_COMPILATION   # Set to False to use tf.function
 USE_JIT_COMPILATION = True  # XLA ignores random seeds. Set to False for reproducibility
