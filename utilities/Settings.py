176114258d9a72c1a38085c9cd0b277b62a50919
Florian Bolli <mail@florianbolli.ch>
2023-04-06 09:00:59 +0200

    new map with appropriate settings

--- utilities/Settings.py
+++ utilities/Settings.py
@@ -60,11 +60,12 @@ class Settings:
     #MAP_CONFIG_FILE =  "utilities/maps_files/config_Montreal.yaml"              #Decrease_resolution_factor = 6   and  ignore_wypts = 2
     #MAP_CONFIG_FILE =  "utilities/maps_files/config_Monza.yaml"                #Decrease_resolution_factor = 6
     #MAP_CONFIG_FILE =  "utilities/maps_files/config_MoscowRaceway.yaml"        #Decrease_resolution_factor = 10
-    MAP_CONFIG_FILE =  "utilities/maps_files/config_Oschersleben.yaml"         #Decrease_resolution_factor = 1
+    # MAP_CONFIG_FILE =  "utilities/maps_files/config_Oschersleben.yaml"         #Decrease_resolution_factor = 1
     #MAP_CONFIG_FILE =  "utilities/maps_files/config_Sakhir.yaml"               #Decrease_resolution_factor = 11
     #MAP_CONFIG_FILE =  "utilities/maps_files/config_SaoPaulo.yaml"             #Decrease_resolution_factor = 9 and ignore_wypts=2
     #MAP_CONFIG_FILE =  "utilities/maps_files/config_Sepang.yaml"               #Decrease_resolution_factor = 6
     #MAP_CONFIG_FILE =  "utilities/maps_files/config_Sochi.yaml"                #Decrease_resolution_factor = 10
+    MAP_CONFIG_FILE =  "utilities/maps/hangar3/config_map_gym.yaml"         #Decrease_resolution_factor = 1
 
 
     # Car parameters
@@ -80,11 +81,11 @@ class Settings:
     SAVE_PLOTS = True # Only possible when SAVE_RECORDINGS is True
     
     #Set Noise Level
-    NOISE_LEVEL_TRANSLATIONAL_CONTROL = 0 # ftg: 0.5  # mppi: 2.0
-    NOISE_LEVEL_ANGULAR_CONTROL = 0  # ftg: 0.05  # mppi: 3.0
+    NOISE_LEVEL_TRANSLATIONAL_CONTROL = 0.1 # ftg: 0.5  # mppi: 2.0
+    NOISE_LEVEL_ANGULAR_CONTROL = 0.1  # ftg: 0.05  # mppi: 3.0
     
-    NOISE_LEVEL_CAR_STATE = [ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    # NOISE_LEVEL_CAR_STATE = [ 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+    NOISE_LEVEL_CAR_STATE = [ 0.07, 0.07, 0.07, 0.07, 0.07, 0.07, 0.07, 0.07, 0.07]
+    # NOISE_LEVEL_CAR_STATE = [ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
     # Automatically follow the first car on the map
     CAMERA_AUTO_FOLLOW = False
