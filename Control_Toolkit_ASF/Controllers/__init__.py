53e349a9c84e2f65dea0e7c7ec9a6b240be404df
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2023-05-22 21:02:41 +0200

    Inverting moving process observation into planner_template

--- Control_Toolkit_ASF/Controllers/__init__.py
+++ Control_Toolkit_ASF/Controllers/__init__.py
@@ -3,13 +3,6 @@ import numpy as np
 
 from utilities.lidar_utils import LidarHelper
 
-from utilities.state_utilities import (
-    POSE_THETA_IDX,
-    POSE_X_IDX,
-    POSE_Y_IDX,
-)
-
-
 class template_planner(ABC):
     def __init__(self):
 
@@ -25,10 +18,9 @@ class template_planner(ABC):
     def set_car_state(self, car_state):
         self.car_state = np.array(car_state).astype(np.float32)
 
+    @abstractmethod
     def process_observation(self, ranges=None, ego_odom=None):
+        pass
 
-        self.LIDAR.load_lidar_measurement(ranges)
-        self.lidar_points = self.LIDAR.get_processed_lidar_points_in_map_coordinates(
-            self.car_state[POSE_X_IDX], self.car_state[POSE_Y_IDX], self.car_state[POSE_THETA_IDX]
-        )
+
 
