b56d7dc6ab5863d25edf10a0a50e6773b14d47df
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2023-05-23 12:37:03 +0200

    data augmentation added to train with corrupted lidar scans

---
+++ SI_Toolkit_ASF/data_augmentation.py
@@ -0,0 +1,17 @@
+from utilities.lidar_utils import LidarHelper
+from tqdm import trange
+from time import sleep
+
+Lidar = LidarHelper()
+CORRUPT_LIDAR_FOR_TRAINING = True
+
+
+def augment_data(data, labels):
+
+    if CORRUPT_LIDAR_FOR_TRAINING:
+        print('Augmenting data...')
+        sleep(0.002)
+        for i in trange(len(data)):
+            data[i] = Lidar.corrupt_datafile(data[i])
+
+    return data, labels
