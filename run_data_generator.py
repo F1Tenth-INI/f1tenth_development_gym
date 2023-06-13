bca56b44cf2d9443d308d8a885b316ee8f031e76
Gianni Gugolz <ggugolz@ethz.ch>
2023-06-13 10:09:38 +0200

    Refactoring data generator

--- run_data_generator.py
+++ run_data_generator.py
@@ -1,4 +1,3 @@
-import sys
 import os
 
 import tensorflow as tf
@@ -15,7 +14,6 @@ from SI_Toolkit.computation_library import TensorFlowLibrary
 from DataGen.Utilities import get_initial_states
 from utilities.Recorder import create_csv_header
 
-from datetime import datetime
 
 try:
     # Use gitpython to get a current revision number and use it in description of experimental data
@@ -23,66 +21,13 @@ try:
 except:
     pass
 
-
-
-def run_data_generator(run_for_ML_Pipeline=False, record_path=None):
-
-    # TODO: (Get rid of for loop)
-    # TODO: (Save file as in Cartpole -> with split for train test, validate)
-    # TODO: (State initialisation - what not loaded, randomly initialized as in CP)
-    # TODO: Interpolator
-
-    # Custom TODOs
-    # TODO: (Parallelize this. Since evey experiment writes to his own file, this should not be an issue) --> As paralellized as generator itself is
-    # TODO: (Switch to pandas earlier, makes things much more readable imo)
-    # TODO: Data handling/deleting is a problem for training with missing steps
-    # TODO: (Fix wrong index in file naming (start new in new folder))
-
-
-    config = yaml.load(open('DataGen/config_data_gen.yml', "r"), Loader=yaml.FullLoader)
-
-    predictor = PredictorWrapper()
-    number_of_initial_states = config['number_of_initial_states']
-    trajectory_length = config['trajectory_length']
-    number_of_trajectories_per_initial_state = config['number_of_trajectories_per_initial_state']
-    dt = config['dt']  # Saving & Control Update
-
-    strategy = config['control_inputs']['strategy']
-
-    path_save_to = config['path_save_to']
-
-    load_initial_states_from_file = config['initial_states']['from_file']['load_from_file']
-    if load_initial_states_from_file:
-        initial_states_from_file_features = config['initial_states']['from_file']['features']
-        file_with_initial_states = config['initial_states']['from_file']['file_with_initial_states']
-    else:
-        initial_states_from_file_features = None
-        file_with_initial_states = None
-
-    random_initial_states = config['initial_states']['random']
-
-    np.random.seed(config['seed'])
-
-
-    data = None
-    states, number_of_initial_states = get_initial_states(number_of_initial_states, random_initial_states, 
-                                                          file_with_initial_states, initial_states_from_file_features)
-    states = np.repeat(states, repeats=number_of_trajectories_per_initial_state, axis=0)
-    total_number_of_trajectories = number_of_initial_states * number_of_trajectories_per_initial_state
-    
-    predictor.configure(
-        batch_size=total_number_of_trajectories,
-        horizon=trajectory_length-1,  # Number of Steps per Trajectory: the trajectory length include also initial state
-        dt=dt,
-        computation_library=TensorFlowLibrary,
-        predictor_specification="ODE_TF_default"
-    )
-
+def generate_random_inputs(config, total_number_of_trajectories, trajectory_length):
     # ----> Angular control
     mu_ac, sigma_ac = config['control_inputs']['angular_control_range']  # mean and standard deviation
     # ----> Translational control
     # mu_tc, sigma_tc = 0, 0.5  # mean and standard deviation
     min_tc, max_tc = config['control_inputs']['translational_control_range']
+    strategy = config['control_inputs']['strategy']
 
     if strategy == 'constant':
         # ---------------Constant input control for each trajectory-------------------------
@@ -96,26 +41,57 @@ def run_data_generator(run_for_ML_Pipeline=False, record_path=None):
 
     elif strategy == 'random':
         # ----------------Random input control for each trajectory-------------------------
-        u_angular = np.random.normal(mu_ac, sigma_ac, total_number_of_trajectories*trajectory_length + 1)
-        u_translational = np.random.uniform(min_tc, max_tc, total_number_of_trajectories*trajectory_length + 1)
+        u_angular = np.random.normal(mu_ac, sigma_ac, total_number_of_trajectories*trajectory_length)
+        u_translational = np.random.uniform(min_tc, max_tc, total_number_of_trajectories*trajectory_length)
         
-        # TODO: Decide if this filter is good?
-        u_angular = np.convolve(u_angular, np.ones(2), 'valid') / 2
-        u_translational =  np.convolve(u_translational, np.ones(2), 'valid') / 2
+        # TODO: Decide if this filter is good? --> Add +1 to length above to use!
+        #u_angular = np.convolve(u_angular, np.ones(2), 'valid') / 2
+        #u_translational =  np.convolve(u_translational, np.ones(2), 'valid') / 2
         
         # Each row of controls is a control input to be followed along a trajectory for the corresponding initial state
         controls = np.column_stack((u_angular, u_translational)) # dim = [initial_states, 2]
         control = controls.reshape(total_number_of_trajectories, trajectory_length, 2) # dim = [initial_states,trajectory_length,2]
     else:
         raise Exception('{} is not a valid strategy for generating control inputs'.format(strategy))
-    
 
-    s = tf.convert_to_tensor(states.reshape([total_number_of_trajectories, -1]), dtype=tf.float32)
-    Q = tf.convert_to_tensor(control.reshape(total_number_of_trajectories, trajectory_length, 2), dtype=tf.float32)[:, :-1, :] # We don't need last control inputs, they will not be applied
-    
-    print('Generating data...')
-    predictions = np.array(predictor.predict_tf(s, Q))  # dim = [total_trajectories, trajectory_length, 9]
+    return control
 
+def save_dataframe_to_csv(df, config, run_for_ML_Pipeline, record_path, total_number_of_trajectories):
+    index = 0
+    if run_for_ML_Pipeline:
+        path = record_path + '/Train/'
+    elif record_path is None:
+        path = config["path_save_to"]
+    else:
+        path = record_path
+
+    strategy = config['control_inputs']['strategy']
+    dt = config['dt']
+
+    print('Saving data...')
+    for experiment in trange(total_number_of_trajectories):
+        if run_for_ML_Pipeline:
+            split = config['split']
+            if experiment == int(split[0]*total_number_of_trajectories):
+                path = record_path + "/Validate/"
+                index = 0
+            elif experiment == int((split[0]+split[1])*total_number_of_trajectories):
+                path = record_path + "/Test/"
+                index = 0
+
+            try:
+                os.makedirs(path)
+            except:
+                pass
+
+        csv_path = f'{path}Trajectory_{strategy}-{str(index)}.csv'
+        if not df[df.experiment_index == experiment].empty:
+            csv_path = create_csv_header(path, controller_name='Random input', dt=dt, csv_name=csv_path)
+            df[df.experiment_index == experiment].to_csv(csv_path, index=False, mode='a')
+        index += 1
+
+def prediction_to_df(predictions, control, config, total_number_of_trajectories, trajectory_length):
+    dt = config['dt']
     data = np.concatenate((control, predictions), axis=2)
     grid = np.mgrid[0:data.shape[0], 0:data.shape[1]]
     time = grid[1][:, :, np.newaxis] * dt
@@ -140,47 +116,67 @@ def run_data_generator(run_for_ML_Pipeline=False, record_path=None):
                     ]
 
     df = pd.DataFrame(data, columns=column_names)
+    return (data, df)
 
-    # Sort out experiments with anomalous angular velocity
+def clean_dataframe(df):
     idx = df.index[df['angular_vel_z'].abs() > 25]
     problematic_experiments = df.loc[idx]['experiment_index'].unique()
     print(f'Found {len(problematic_experiments)} problematic experiments!: {problematic_experiments}')
-    #df = df[~(df.experiment_index.isin(problematic_experiments))].reset_index(drop=True)
+    df = df[~(df.experiment_index.isin(problematic_experiments))].reset_index(drop=True)
 
-    # Compute the number of nan in the data
+    return df
+
+def get_initial_state_configuration(config):
+    load_initial_states_from_file = config['initial_states']['from_file']['load_from_file']
+    if load_initial_states_from_file:
+        initial_states_from_file_features = config['initial_states']['from_file']['features']
+        file_with_initial_states = config['initial_states']['from_file']['file_with_initial_states']
+    else:
+        initial_states_from_file_features = None
+        file_with_initial_states = None
+
+    return (initial_states_from_file_features, file_with_initial_states)
+
+def run_data_generator(run_for_ML_Pipeline=False, record_path=None):
+
+    # TODO: Interpolator
+
+    config = yaml.load(open('DataGen/config_data_gen.yml', "r"), Loader=yaml.FullLoader)
+    number_of_initial_states = config['number_of_initial_states']
+    trajectory_length = config['trajectory_length']
+    number_of_trajectories_per_initial_state = config['number_of_trajectories_per_initial_state']
+    
+    initial_states_from_file_features, file_with_initial_states = get_initial_state_configuration(config)
+    random_initial_states = config['initial_states']['random']
+
+    np.random.seed(config['seed'])
+
+    states, number_of_initial_states = get_initial_states(number_of_initial_states, random_initial_states, 
+                                                          file_with_initial_states, initial_states_from_file_features)
+    states = np.repeat(states, repeats=number_of_trajectories_per_initial_state, axis=0)
+    total_number_of_trajectories = number_of_initial_states * number_of_trajectories_per_initial_state
+    
+    predictor = PredictorWrapper()
+    predictor.configure(
+        batch_size=total_number_of_trajectories,
+        horizon=trajectory_length-1,  # Number of Steps per Trajectory: the trajectory length include also initial state
+        dt=config['dt'],
+        computation_library=TensorFlowLibrary,
+        predictor_specification="ODE_TF_default"
+    )
+
+    control = generate_random_inputs(config, total_number_of_trajectories, trajectory_length)
+    initial_states_tf = tf.convert_to_tensor(states.reshape([total_number_of_trajectories, -1]), dtype=tf.float32)
+    control_tf = tf.convert_to_tensor(control.reshape(total_number_of_trajectories, trajectory_length, 2), dtype=tf.float32)[:, :-1, :] # We don't need last control inputs, they will not be applied
+    
+    print('Generating data...')
+    predictions = np.array(predictor.predict_tf(initial_states_tf, control_tf))  # dim = [total_trajectories, trajectory_length, 9] 
+    data, df = prediction_to_df(predictions, control, config, total_number_of_trajectories, trajectory_length)
+
+    df = clean_dataframe(df)
     print('Number of nan element in the generated Data:', np.count_nonzero(np.isnan(data)))
 
-    
-    index = 0
-    if run_for_ML_Pipeline:
-        path = record_path + '/Train/'
-    elif record_path is None:
-            path = config["path_save_to"]
-    else:
-        path = record_path
-    
-    print('Saving data...')
-    for experiment in trange(total_number_of_trajectories):
-        if run_for_ML_Pipeline:
-            split = config['split']
-            if experiment == int(split[0]*total_number_of_trajectories):
-                path = record_path + "/Validate/"
-                index = 0
-            elif experiment == int((split[0]+split[1])*total_number_of_trajectories):
-                path = record_path + "/Test/"
-                index = 0
-
-            try:
-                os.makedirs(path)
-            except:
-                pass
-        
-        csv_path = f'{path}Trajectory_{strategy}-{str(index)}.csv'
-        if not df[df.experiment_index == experiment].empty:
-            csv_path = create_csv_header(path, controller_name='Random input', dt=dt, csv_name=csv_path)
-            df[df.experiment_index == experiment].to_csv(csv_path, index=False, mode='a')
-        index += 1
-
+    save_dataframe_to_csv(df, config, run_for_ML_Pipeline, record_path, total_number_of_trajectories)
     print('Finished data generation')
 
 
