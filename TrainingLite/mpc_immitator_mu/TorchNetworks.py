1ff4e4c86bb60322eb931bff79b79f22374f3fa0
Florian Bolli <mail@florianbolli.ch>
2024-10-24 13:50:03 +0200

    training custom networks with torch

---
+++ TrainingLite/mpc_immitator_mu/TorchNetworks.py
@@ -0,0 +1,46 @@
+import torch
+from torch import nn
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_layers=2):
+        super(LSTM, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+        
+        self.hidden = self.reset_hidden_state(1)
+
+    def forward(self, x, hidden):
+        out, hidden = self.lstm(x, hidden)
+        out = self.fc(out[:, -1, :])  # Take the output at the last time step
+        return out, hidden
+
+    def reset_hidden_state(self, batch_size):
+        device = next(self.parameters()).device
+        return (torch.zeros(self.num_layers, batch_size, self.hidden_size).to(device),
+                torch.zeros(self.num_layers, batch_size, self.hidden_size).to(device))
+        
+        
+class GRU(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, num_layers=2):
+        super(GRU, self).__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+        
+        self.hidden = self.reset_hidden_state(1)
+
+    def forward(self, x, hidden):
+        out, hidden = self.gru(x, hidden)
+        out = self.fc(out[:, -1, :])  # Take the output at the last time step
+        return out, hidden
+
+    def reset_hidden_state(self, batch_size):
+        device = next(self.parameters()).device
+        return torch.zeros(self.num_layers, batch_size, self.hidden_size).to(device)
