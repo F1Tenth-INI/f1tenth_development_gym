9b6ec1a258cfb9e00a564722f28209aaffd589b6
Florian Bolli <mail@florianbolli.ch>
2024-10-24 09:58:46 +0200

    lstm and cleanup with trainingHelper

--- TrainingLite/mpc_immitator_mu/torch_predict.py
+++ TrainingLite/mpc_immitator_mu/torch_predict.py
@@ -18,84 +18,64 @@ import yaml
 from utilities.state_utilities import *
 from utilities.waypoint_utils import *
 
-model_name = "tGRU7_b16_files_shuf_mpc2"
+model_name = "tLSTM7_b16_files_shuf_mpc2_reduce_lr"
 
-experiment_path =os.path.dirname(os.path.realpath(__file__))
-
-
-
+experiment_path = os.path.dirname(os.path.realpath(__file__))
 
 # Load the model
-
-# When loading, provide the custom loss function in the `custom_objects` argument
-# custom_objects = {'custom_mse_loss_with_washout': custom_mse_loss_with_washout}
-custom_objects = {}
-
-# Load the model and include the custom objects
-# model = load_model(experiment_path + '/models/'+model_name+'/my_model.keras', custom_objects=custom_objects)
-model_dir = experiment_path + '/models/'+model_name
-input_scaler = load(experiment_path + '/models/'+model_name+'/input_scaler.joblib')
-output_scaler = load(experiment_path + '/models/'+model_name+'/output_scaler.joblib')
-with open(experiment_path + '/models/'+model_name+'/network.yaml', 'r') as file:
+model_dir = experiment_path + '/models/' + model_name
+input_scaler = load(experiment_path + '/models/' + model_name + '/input_scaler.joblib')
+output_scaler = load(experiment_path + '/models/' + model_name + '/output_scaler.joblib')
+with open(experiment_path + '/models/' + model_name + '/network.yaml', 'r') as file:
     network_yaml = yaml.safe_load(file)
-    
-    
 
 mu_history = []
 input_history = []
 
-
-
-
-
 def predict_next_control(s, waypoints_relative, waypoints):
-    
-    # state = [s[ANGULAR_VEL_Z_IDX], s[LINEAR_VEL_X_IDX],s[POSE_THETA_IDX],s[STEERING_ANGLE_IDX]]
-    state = [s[ANGULAR_VEL_Z_IDX], s[LINEAR_VEL_X_IDX],s[POSE_THETA_IDX],s[STEERING_ANGLE_IDX], s[SLIP_ANGLE_IDX]]
-    waypoints_x = waypoints_relative[:,0]
-    waypoints_y = waypoints_relative[:,1]
-    waypoints_vx = waypoints[:,WP_VX_IDX]
-    
-    input = np.concatenate((state, waypoints_x, waypoints_y, waypoints_vx))
-    
+    state = [s[ANGULAR_VEL_Z_IDX], s[LINEAR_VEL_X_IDX], s[POSE_THETA_IDX], s[STEERING_ANGLE_IDX], s[SLIP_ANGLE_IDX]]
+    waypoints_x = waypoints_relative[:, 0]
+    waypoints_y = waypoints_relative[:, 1]
+    waypoints_vx = waypoints[:, WP_VX_IDX]
 
+    input = np.concatenate((state, waypoints_x, waypoints_y, waypoints_vx))
     output = predict(input)
-    
     control = [output]
 
-    
     return control
 
-# Recreate the model class
-class GRUNetwork(nn.Module):
+# Recreate the model class with LSTM
+class Network(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, num_layers=2):
-        super(GRUNetwork, self).__init__()
+        super(Network, self).__init__()
         self.num_layers = num_layers
         self.hidden_size = hidden_size
-        self.gru1 = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
         
         self.hidden = self.reset_hidden_state(1)
 
     def forward(self, x, hidden):
-        out, hidden = self.gru1(x, hidden)
+        out, hidden = self.lstm(x, hidden)
         out = self.fc(out[:, -1, :])  # Take the output at the last time step
         return out, hidden
 
     def reset_hidden_state(self, batch_size):
         device = next(self.parameters()).device
-        return torch.zeros(self.num_layers, batch_size, self.hidden_size).to(device)
+        return (torch.zeros(self.num_layers, batch_size, self.hidden_size).to(device),
+                torch.zeros(self.num_layers, batch_size, self.hidden_size).to(device))
 
 # Specify the model parameters (same as during training)
-input_size =len(network_yaml["input_cols"]) # Number of input features
-output_size = len(network_yaml["output_cols"]) # Number of output features
+input_size = len(network_yaml["input_cols"])  # Number of input features
+output_size = len(network_yaml["output_cols"])  # Number of output features
 hidden_size = 64
 num_layers = 2
+
 # Create an instance of the model
-model = GRUNetwork(input_size, hidden_size, output_size, num_layers)
+model = Network(input_size, hidden_size, output_size, num_layers)
 
 # Load the saved model state dictionary
-model.load_state_dict(torch.load(os.path.join(model_dir, "gru_model.pth")))
+model.load_state_dict(torch.load(os.path.join(model_dir, "model.pth")))
 
 # Transfer the model to the appropriate device (GPU or CPU)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -103,8 +83,6 @@ model = model.to(device)
 
 # Set the model to evaluation mode
 model.eval()
-
-
 
 def predict(input):
     # Assume `X_new` is the new input data you want to make predictions on
@@ -118,11 +96,6 @@ def predict(input):
     # Move the tensor to the same device as the model
     X_new_tensor = X_new_tensor.to(next(model.parameters()).device)
 
-
-    # Reset the hidden state for the new input
-    # hidden = model.reset_hidden_state(X_new_tensor.size(0))
-    # hidden = model.init_hidden_state(1)
-
     # Perform inference
     with torch.no_grad():
         output, model.hidden = model(X_new_tensor, model.hidden)
@@ -132,12 +105,14 @@ def predict(input):
     return predictions[0]
 
 if __name__ == "__main__":
-    
-    print("inputs" , network_yaml["input_cols"])
+    print("inputs", network_yaml["input_cols"])
     print("outputs", network_yaml["output_cols"])
-    
-    example_input = np.array([0, 0., 0., 0.3232363, 0., 18.092684, 18.492521, 18.892359, 19.292198, 19.692036, 20.091873, 20.491713, 20.89155, 21.29139, 21.691227, 22.091066, 22.490904, 22.890741, 23.29058, 23.690418, 24.090258, 24.490095, 24.889935, 25.289772, 25.68961, -12.683541, -12.289727, -11.892686, -11.494469, -11.096025, -10.697667, -10.299485, -9.90103, -9.501698, -9.101922, -8.703446, -8.309871, -7.927363, -7.565548, -7.238788, -6.967873, -6.785771, -6.733249, -6.79366, -6.926713, 5.158623, 5.227309, 5.27428, 5.310179, 5.343515, 5.377885, 5.414228, 5.447311, 5.466889, 5.464843, 5.433206, 5.363721, 5.248224, 5.079041, 4.849722, 4.556875, 4.203009, 3.808486, 3.414118, 3.037381])    # should return -0.070	-0.013
-    
+
+    example_input = np.array([0, 0., 0., 0.3232363, 0., 18.092684, 18.492521, 18.892359, 19.292198, 19.692036, 20.091873, 20.491713, 20.89155, 21.29139, 21.691227, 22.091066, 22.490904, 22.890741, 23.29058, 23.690418, 24.090258, 24.490095, 24.889935, 25.289772, 25.68961, -12.683541, -12.289727, -11.892686, -11.494469, -11.096025, -10.697667, -10.299485, -9.90103, -9.501698, -9.101922, -8.703446, -8.309871, -7.927363, -7.565548, -7.238788, -6.967873, -6.785771, -6.733249, -6.79366, -6.926713, 5.158623, 5.227309, 5.27428, 5.310179, 5.343515, 5.377885, 5.414228, 5.447311, 5.466889, 5.464843, 5.433206, 5.363721, 5.248224, 5.079041, 4.849722, 4.556875, 4.203009, 3.808486, 3.414118, 3.037381])  # should return -0.070 -0.013
+
+    # Initialize hidden state for prediction
+    model.hidden = model.reset_hidden_state(1)
+
     for i in range(21):
         prediction = predict(example_input)
     print(prediction)
