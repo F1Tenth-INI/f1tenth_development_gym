9871c8cb00c417128134888df8f4b249ee0cc64b
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2024-12-17 16:27:59 +0100

    New default settings to connect to NUC

--- RaceTuner/TunerSettings.py
+++ RaceTuner/TunerSettings.py
@@ -1,9 +1,9 @@
 # TunerSettings.py
 import os
 
-USE_REMOTE_FILES = False
+USE_REMOTE_FILES = True
 
-MAP_NAME = "RCA2"  # Replace as needed
+MAP_NAME = "Milan1"  # Replace as needed
 LOCAL_MAP_DIR = "utilities/maps"
 REMOTE_MAP_DIR = "catkin_ws/src/f1tenth_system/racecar/racecar/maps"
 REMOTE_AT_LOCAL_DIR = "./maps/"
@@ -11,7 +11,7 @@ REMOTE_AT_LOCAL_DIR = "./maps/"
 REVERSE_DIRECTION = False
 
 REMOTE_CONFIG = {
-    "host": os.getenv("REMOTE_HOST", "ini-xavier.local"),
+    "host": os.getenv("REMOTE_HOST", "ini-nuc.local"),
     "port": int(os.getenv("REMOTE_PORT", "22")),
     "username": os.getenv("REMOTE_USERNAME", "racecar"),
     "password": os.getenv("REMOTE_PASSWORD", "Inivincible"),
