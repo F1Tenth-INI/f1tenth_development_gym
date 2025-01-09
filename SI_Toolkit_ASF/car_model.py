cb179489e8f9ef9e14bbd4be7b39da80037ce786
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2025-01-09 11:57:50 +0100

    Modularization

--- SI_Toolkit_ASF/car_model.py
+++ SI_Toolkit_ASF/car_model.py
@@ -69,28 +69,26 @@ class car_model:
         beta = s[:, SLIP_ANGLE_IDX]
         delta = s[:, STEERING_ANGLE_IDX]
 
-        delta_dot, v_x_dot = self.lib.unstack(Q, 2, 1)
+        delta_dot_cmd, v_x_dot_cmd = self.lib.unstack(Q, 2, 1)
         
         # Constraints
-        v_x_dot = self.accl_constraints(v_x, v_x_dot)
-        delta_dot = self.steering_constraints(delta, delta_dot)
+        v_x_dot_cmd = self.accl_constraints(v_x, v_x_dot_cmd)
+        delta_dot_cmd = self.steering_constraints(delta, delta_dot_cmd)
 
         # Euler stepping
         for _ in range(self.intermediate_steps):
-            s_x_dot = v_x * self.lib.cos(psi)
-            s_y_dot = v_x * self.lib.sin(psi)
-            # delta_dot = delta_dot
-            # v_x_dot = v_x_dot
-            psi_dot = (v_x / self.car_parameters.l_wb) * self.lib.tan(delta)
-            psi_dot_dot = (v_x_dot * self.lib.tan(delta) / self.car_parameters.l_wb) \
-                + v_x * delta_dot / (self.car_parameters.l_wb * self.lib.cos(delta) ** 2)
+
+            s_x_dot, s_y_dot, psi_dot, v_x_dot, psi_dot_dot, delta_dot \
+                = self._ks_model(psi, v_x, delta, v_x_dot_cmd, delta_dot_cmd)
 
             s_x = s_x + self.t_step * s_x_dot
             s_y = s_y + self.t_step * s_y_dot
-            delta = self.lib.clip(delta + self.t_step * delta_dot, self.car_parameters.s_min, self.car_parameters.s_max)
+            delta = delta + self.t_step * delta_dot
             v_x = v_x + self.t_step * v_x_dot
             psi = psi + self.t_step * psi_dot
             angular_vel_z = angular_vel_z + self.t_step * psi_dot_dot
+
+            delta = self.lib.clip(delta, self.car_parameters.s_min, self.car_parameters.s_max)
 
         linear_vel_x = v_x
         pose_theta_cos = self.lib.cos(psi)
@@ -115,7 +113,63 @@ class car_model:
                                      slip_angle,
                                      delta)
 
-   
+    def _ks_model(self,
+                  psi,  # Heading angle
+                  v_x,  # Derivatives of state
+                  delta,  # Steering angle
+                  v_x_dot_cmd, delta_dot_cmd,  # Control inputs
+                  ):
+
+        s_x_dot = v_x * self.lib.cos(psi)
+        s_y_dot = v_x * self.lib.sin(psi)
+        delta_dot = delta_dot_cmd
+        v_x_dot = v_x_dot_cmd
+        psi_dot = (v_x / self.car_parameters.l_wb) * self.lib.tan(delta)
+        psi_dot_dot = (v_x_dot * self.lib.tan(delta) / self.car_parameters.l_wb) \
+                        + v_x * delta_dot / (self.car_parameters.l_wb * self.lib.cos(delta) ** 2)
+
+        return (
+            s_x_dot, s_y_dot,  # Rate of change of position
+            psi_dot,  # Rate of change of heading angle
+
+            v_x_dot,  # Rate of change of velocity
+            psi_dot_dot,  # Rate of change of angular velocity
+
+            delta_dot,  # Rate of change of steering angle
+        )
+
+    def _pacejka_model(
+            self,
+            psi, v_x, v_y, psi_dot, delta, delta_dot, v_x_dot,  # Car state
+            mu, lf, lr, h_cg, m, I_z, g_,  # Car parameters
+            B_f, C_f, D_f, E_f, B_r, C_r, D_r, E_r,  # Pacejka parameters
+    ):
+
+        v_x = self.lib.where(v_x == 0, self.lib.constant(1e-8, self.lib.float32), v_x)
+        alpha_f = -self.lib.atan((v_y + psi_dot * lf) / (v_x)) + delta
+        alpha_r = -self.lib.atan((v_y - psi_dot * lr) / v_x)
+
+        # compute vertical tire forces
+        F_zf = m * (-v_x_dot * h_cg + g_ * lr) / (lr + lf)
+        F_zr = m * (v_x_dot * h_cg + g_ * lf) / (lr + lf)
+
+        F_yf = mu * F_zf * D_f * self.lib.sin(
+            C_f * self.lib.atan(B_f * alpha_f - E_f * (B_f * alpha_f - self.lib.atan(B_f * alpha_f))))
+        F_yr = mu * F_zr * D_r * self.lib.sin(
+            C_r * self.lib.atan(B_r * alpha_r - E_r * (B_r * alpha_r - self.lib.atan(B_r * alpha_r))))
+
+        d_pos_x = v_x * self.lib.cos(psi) - v_y * self.lib.sin(psi)
+        d_pos_y = v_x * self.lib.sin(psi) + v_y * self.lib.cos(psi)
+        d_psi = psi_dot
+        d_v_x = v_x_dot
+        d_v_y = 1 / m * (F_yr + F_yf) - v_x * psi_dot
+        # print d_v_y + v_x * psi_dot
+        # Should be equal to IMUs a_y
+
+        d_psi_dot = 1 / I_z * (-lr * F_yr + lf * F_yf)
+
+        return d_pos_x, d_pos_y, d_psi, d_v_x, d_v_y, d_psi_dot, delta_dot
+
 
     def _step_dynamics_pacejka(self, s, Q):
         
@@ -156,26 +210,12 @@ class car_model:
         delta_dot = self.steering_constraints(delta, delta_dot)
 
         for _ in range(self.intermediate_steps):
-            v_x = self.lib.where(v_x == 0, self.lib.constant(1e-8, self.lib.float32), v_x)
-            alpha_f = -self.lib.atan((v_y + psi_dot * lf) / (v_x)) + delta
-            alpha_r = -self.lib.atan((v_y - psi_dot * lr) / v_x )
-
-            # compute vertical tire forces
-            F_zf = m * (-v_x_dot * h_cg + g_ * lr) / (lr + lf)
-            F_zr = m * (v_x_dot * h_cg + g_ * lf) / (lr + lf)
-
-            F_yf = mu * F_zf * D_f * self.lib.sin(C_f * self.lib.atan(B_f * alpha_f - E_f*(B_f * alpha_f - self.lib.atan(B_f * alpha_f))))
-            F_yr = mu * F_zr * D_r * self.lib.sin(C_r * self.lib.atan(B_r * alpha_r - E_r*(B_r * alpha_r - self.lib.atan(B_r * alpha_r))))
-
-            d_pos_x = v_x * self.lib.cos(psi) - v_y * self.lib.sin(psi)
-            d_pos_y = v_x * self.lib.sin(psi) + v_y * self.lib.cos(psi)
-            d_psi = psi_dot
-            d_v_x = v_x_dot
-            d_v_y = 1/m * (F_yr + F_yf) - v_x * psi_dot
-            # print d_v_y + v_x * psi_dot
-            # Should be equal to IMUs a_y
-            
-            d_psi_dot = 1/I_z * (-lr * F_yr + lf * F_yf)
+            d_pos_x, d_pos_y, d_psi, d_v_x, d_v_y, d_psi_dot, d_delta \
+                = self._pacejka_model(
+                psi, v_x, v_y, psi_dot, delta, delta_dot, v_x_dot,
+                mu, lf, lr, h_cg, m, I_z, g_,
+                B_f, C_f, D_f, E_f, B_r, C_r, D_r, E_r,
+            )
 
             s_x = s_x + self.t_step * d_pos_x
             s_y = s_y + self.t_step * d_pos_y
