f4b4a329887b5809c7379119070c581c85accb6a
Marcin Paluch Euler <marcin.paluch1994@gmail.com>
2025-03-04 01:55:50 +0100

    Reducing amount of data by half, it will be anyway double the usual amount

--- scripts/EulerCluster/ControllerAlongTrajectories.sh
+++ scripts/EulerCluster/ControllerAlongTrajectories.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --array=1-378            # Create an array job with task IDs
+# SBATCH --array=1-756            # Create an array job with task IDs 1-756
 #SBATCH --mem-per-cpu=3G         # Request memory per CPU
 #SBATCH --cpus-per-task=1        # Assign the required number of CPUs per task
 #SBATCH --time=2:00:00           # Set the maximum job time
