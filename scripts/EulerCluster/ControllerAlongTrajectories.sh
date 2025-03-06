6c7a429b8a8cf53f58fc87d52439046d2f29ab52
Marcin Paluch Euler <marcin.paluch1994@gmail.com>
2025-03-06 10:01:50 +0100

    Control along trajectories script updated

--- scripts/EulerCluster/ControllerAlongTrajectories.sh
+++ scripts/EulerCluster/ControllerAlongTrajectories.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --array=1-756            # Create an array job with task IDs 1-756
-#SBATCH --mem-per-cpu=3G         # Request memory per CPU
+#SBATCH --mem-per-cpu=2G         # Request memory per CPU
 #SBATCH --cpus-per-task=1        # Assign the required number of CPUs per task
 #SBATCH --time=2:00:00           # Set the maximum job time
 #SBATCH --output=./scripts/EulerCluster/out/slurm-%A_%a.out   # Output file
@@ -14,9 +14,6 @@ conda activate f1t
 export PYTHONPATH=/cluster/home/paluchm/f1tenth_development_gym/
 cd $HOME/f1tenth_development_gym/
 
-# Use SLURM_ARRAY_TASK_ID for the model index directly since it ranges from 1 to 50
-i=$SLURM_ARRAY_TASK_ID
-
 # Run the Python script with the specific index
-python ./SI_Toolkit_ASF/run/PreprocessData_Add_Control_Along_Trajectories.py -i $i
+python ./SI_Toolkit_ASF/run/PreprocessData_Add_Control_Along_Trajectories.py -i $SLURM_ARRAY_TASK_ID
 
