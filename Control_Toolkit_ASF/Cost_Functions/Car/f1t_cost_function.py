ddbaf87b543e958744e25f40f2eedd93abb7a444
marcinpaluch1994 <marcin.paluch1994@gmail.com>
2025-05-14 22:20:36 +0200

    Removing direct references to tensorflow from f1t cost-functions

--- Control_Toolkit_ASF/Cost_Functions/Car/f1t_cost_function.py
+++ Control_Toolkit_ASF/Cost_Functions/Car/f1t_cost_function.py
@@ -1,6 +1,5 @@
 import yaml
 import os
-import tensorflow as tf
 from types import SimpleNamespace
 
 from Control_Toolkit.Cost_Functions import cost_function_base
@@ -70,39 +69,39 @@ class f1t_cost_function(cost_function_base):
         return self._P1
 
     @property
-    def P1_tf(self):
-        return self._P1_tf
+    def P1_var(self):
+        return self._P1_var
 
     @P1.setter
     def P1(self, P1):
         self._P1 = P1
-        if not hasattr(self, "_P1_tf"):
-            self._P1_tf = self.lib.to_variable(P1, self.lib.float32)
+        if not hasattr(self, "_P1_var"):
+            self._P1_var = self.lib.to_variable(P1, self.lib.float32)
         else:
-            self._P1_tf.assign(P1)
+            self._P1_var.assign(P1)
 
     @property
     def P2(self):
         return self._P2
 
     @property
-    def P2_tf(self):
-        return self._P2_tf
+    def P2_var(self):
+        return self._P2_var
 
     @P2.setter
     def P2(self, P2):
         self._P2 = P2
-        if not hasattr(self, "_P2_tf"):
-            self._P2_tf = self.lib.to_variable(P2, self.lib.float32)
+        if not hasattr(self, "_P2_var"):
+            self._P2_var = self.lib.to_variable(P2, self.lib.float32)
         else:
-            self._P2_tf.assign(P2)
+            self._P2_var.assign(P2)
 
     # endregion
 
     # TODO: Make it library agnostic. This also justifies why some methods are not static, although currently they could be
     def get_actuation_cost(self, u):
         cc_cost = R * (u ** 2)
-        return self.lib.sum(cc_weight * cc_cost, -1)
+        return self.lib.sum(cc_weight * cc_cost, axis=-1)
 
     def get_terminal_speed_cost(self, terminal_state):
         ''' Compute penality for deviation from desired max speed'''
@@ -126,7 +125,7 @@ class f1t_cost_function(cost_function_base):
         u_prev_vec = self.lib.concat((self.lib.ones((s0, 1, s2)) * u_prev, u[:, :-1, :]), 1)
         ccrc = ((u - u_prev_vec)/control_limits_max_abs) ** 2
 
-        return self.lib.sum(ccrc_weight * ccrc, -1)
+        return self.lib.sum(ccrc_weight * ccrc, axis=-1)
 
     def get_control_change_rate_within_horizon_cost(self, u):
         """
@@ -140,8 +139,8 @@ class f1t_cost_function(cost_function_base):
         control_diff = self.lib.abs(u[:, 1:, :] - u[:, :-1, :])
         control_diff = self.lib.square(control_diff)
         weighted_control_diff = control_diff * ccrh_weight
-        control_change = tf.reduce_sum(weighted_control_diff, axis=-1) # sum over the control inputs     
-        control_change_padded = tf.pad(control_change, [[0, 0], [1, 0]]) # pad with zeros to match the shape of u
+        control_change = self.lib.sum(weighted_control_diff, axis=-1) # sum over the control inputs
+        control_change_padded = self.lib.pad(control_change, [[0, 0], [1, 0]]) # pad with zeros to match the shape of u
         
         return control_change_padded
 
@@ -156,14 +155,14 @@ class f1t_cost_function(cost_function_base):
         u_next_vec = self.lib.concat((u[:, 1:, :], u[:, -1:, :]), 1)
         ccocrc = self.lib.abs((u_next_vec + u_prev_vec - 2*u)/control_limits_max_abs)
 
-        return self.lib.sum(ccocrc_weight * ccocrc, 1)
+        return self.lib.sum(ccocrc_weight * ccocrc, axis=1)
 
 
     def get_immediate_control_discontinuity_cost(self, u, u_prev):
         u_prev_vec = self.lib.ones_like(u) * u_prev  # The vector is just to keep dimensions right and scaling with gr
         icdc = (u_prev_vec - u[:, :1, :])**2
 
-        return self.lib.sum(icdc_weight * icdc, -1)
+        return self.lib.sum(icdc_weight * icdc, axis=-1)
 
 
 
@@ -187,60 +186,60 @@ class f1t_cost_function(cost_function_base):
     
     def get_angular_velocity_cost(self, s):
         angular_velovities = s[:, :, ANGULAR_VEL_Z_IDX]
-        angula_velocity_cost = angular_velocity_cost_weight * tf.square(angular_velovities)
-        angula_velocity_cost = tf.cast(angula_velocity_cost, tf.float32)
+        angula_velocity_cost = angular_velocity_cost_weight * self.lib.square(angular_velovities)
+        angula_velocity_cost = self.lib.cast(angula_velocity_cost, self.lib.float32)
         return angula_velocity_cost
 
     def get_slipping_cost(self, s):
         slipping_angles = s[:, :, SLIP_ANGLE_IDX]
-        slipping_cost = slipping_cost_weight * tf.square(slipping_angles)
-        slipping_cost = tf.cast(slipping_cost, tf.float32)
+        slipping_cost = slipping_cost_weight * self.lib.square(slipping_angles)
+        slipping_cost = self.lib.cast(slipping_cost, self.lib.float32)
         return slipping_cost
     
     def get_distance_to_waypoints_per_rollout(self, trajectory_points, waypoints):
-        trajectories_shape = tf.shape(trajectory_points)
+        trajectories_shape = self.lib.shape(trajectory_points)
         number_of_rollouts = trajectories_shape[0]
         number_of_steps = trajectories_shape[1]
-        number_of_waypoints = tf.shape(waypoints)[0]
+        number_of_waypoints = self.lib.shape(waypoints)[0]
 
-        positions_of_trajectories = tf.reshape(trajectory_points, [trajectories_shape[0] * trajectories_shape[1], 2])
+        positions_of_trajectories = self.lib.reshape(trajectory_points, [trajectories_shape[0] * trajectories_shape[1], 2])
         distance_to_waypoints = self.distances_from_list_to_list_of_points(positions_of_trajectories, waypoints)
-        distance_to_waypoints_per_rollout = tf.reshape(distance_to_waypoints,
+        distance_to_waypoints_per_rollout = self.lib.reshape(distance_to_waypoints,
                                                        (number_of_rollouts, number_of_steps, number_of_waypoints))
         return distance_to_waypoints_per_rollout
 
     def get_distances_from_trajectory_points_to_closest_target_point(self, trajectory_points, waypoints):
         distance_to_waypoints_per_rollout = self.get_distance_to_waypoints_per_rollout(trajectory_points, waypoints)
-        min_distance_to_waypoints_per_rollout = tf.reduce_min(distance_to_waypoints_per_rollout, axis=2)
+        min_distance_to_waypoints_per_rollout = self.lib.reduce_min(distance_to_waypoints_per_rollout, axis=2)
         return min_distance_to_waypoints_per_rollout
 
     def get_nearest_waypoints_indices(self, trajectory_points, waypoints):
         distance_to_waypoints_per_rollout = self.get_distance_to_waypoints_per_rollout(trajectory_points, waypoints)
-        indices_nearest_waypoints = tf.argmin(distance_to_waypoints_per_rollout, axis=2)
+        indices_nearest_waypoints = self.lib.argmin(distance_to_waypoints_per_rollout, axis=2)
         return indices_nearest_waypoints
 
     def distances_to_list_of_points(self, point, points2):
-        length = tf.shape(points2)[0]
-        points1 = tf.tile([point], [length, 1])
+        length = self.lib.shape(points2)[0]
+        points1 = self.lib.tile([point], [length, 1])
 
         diff = points2 - points1
-        squared_diff = tf.math.square(diff)
-        squared_dist = tf.reduce_sum(squared_diff, axis=1)
+        squared_diff = self.lib.square(diff)
+        squared_dist = self.lib.sum(squared_diff, axis=1)
         return squared_dist
 
 
     def get_crash_cost(self, trajectories, border_points):
 
-        trajectories_shape = tf.shape(trajectories)
+        trajectories_shape = self.lib.shape(trajectories)
 
-        points_of_trajectories = tf.reshape(trajectories, [trajectories_shape[0] * trajectories_shape[1], 2])
+        points_of_trajectories = self.lib.reshape(trajectories, [trajectories_shape[0] * trajectories_shape[1], 2])
         squared_distances = self.distances_from_list_to_list_of_points(points_of_trajectories, border_points)
 
-        minima = tf.math.reduce_min(squared_distances, axis=1)
+        minima = self.lib.reduce_min(squared_distances, axis=1)
 
-        minima = tf.reshape(minima, [trajectories_shape[0], trajectories_shape[1]])
+        minima = self.lib.reshape(minima, [trajectories_shape[0], trajectories_shape[1]])
 
-        minima = tf.clip_by_value(minima, 0.0, crash_cost_safe_margin)
+        minima = self.lib.clip(minima, 0.0, crash_cost_safe_margin)
 
         cost_for_passing_close = self.hyperbolic_function_for_crash_cost(minima)
 
@@ -251,9 +250,9 @@ class f1t_cost_function(cost_function_base):
         min_distance_to_trajectory_points = \
             self.get_distances_from_trajectory_points_to_closest_target_point(trajectories, target_points)
 
-        min_distance_from_trajectory = tf.math.reduce_min(min_distance_to_trajectory_points, axis=1)
+        min_distance_from_trajectory = self.lib.reduce_min(min_distance_to_trajectory_points, axis=1)
 
-        min_distance_from_trajectory_normed = min_distance_from_trajectory / tf.math.reduce_max(
+        min_distance_from_trajectory_normed = min_distance_from_trajectory / self.lib.reduce_max(
             min_distance_from_trajectory)
 
         return min_distance_from_trajectory_normed
@@ -265,15 +264,15 @@ class f1t_cost_function(cost_function_base):
     def distances_from_list_to_list_of_points(self, points1, points2):
 
         # TODO: Cast both as float
-        points1 = tf.cast(points1, tf.float32)
-        points2 = tf.cast(points2, tf.float32)
+        points1 = self.lib.cast(points1, self.lib.float32)
+        points2 = self.lib.cast(points2, self.lib.float32)
     
-        points1 = tf.expand_dims(points1, 1)
-        points2 = tf.expand_dims(points2, 0)
+        points1 = self.lib.unsqueeze(points1, 1)
+        points2 = self.lib.unsqueeze(points2, 0)
 
         diff = points2 - points1
-        squared_diff = tf.math.square(diff)
-        squared_dist = tf.reduce_sum(squared_diff, axis=2)
+        squared_diff = self.lib.square(diff)
+        squared_dist = self.lib.sum(squared_diff, axis=2)
 
         return squared_dist
 
@@ -284,30 +283,30 @@ class f1t_cost_function(cost_function_base):
         return distance_to_waypoints_cost_weight * self.get_squared_distances_to_nearest_wp_segment(car_positions, waypoint_positions, nearest_waypoint_indices)
 
     def get_wp_segment_vectors(self, waypoint_positions, nearest_waypoint_indices):
-        nearest_waypoints = tf.gather(waypoint_positions, nearest_waypoint_indices)
-        nearest_waypoints_next = tf.gather(waypoint_positions, nearest_waypoint_indices + 1)
+        nearest_waypoints = self.lib.gather(waypoint_positions, nearest_waypoint_indices, axis=0)
+        nearest_waypoints_next = self.lib.gather(waypoint_positions, nearest_waypoint_indices + 1, axis=0)
 
         # Get next waypoint segment vector for every position on the car's rollout
         wp_segment_vectors = nearest_waypoints_next - nearest_waypoints
         return wp_segment_vectors
 
     def get_squared_distances_to_nearest_wp_segment(self, car_positions, waypoint_positions, nearest_waypoint_indices):
-        nearest_waypoints = tf.gather(waypoint_positions, nearest_waypoint_indices)
-        nearest_waypoints_next = tf.gather(waypoint_positions, nearest_waypoint_indices + 1)
+        nearest_waypoints = self.lib.gather(waypoint_positions, nearest_waypoint_indices, axis=0)
+        nearest_waypoints_next = self.lib.gather(waypoint_positions, nearest_waypoint_indices + 1, axis=0)
 
         # Get next waypoint segment vector for every position on the car's rollout
         wp_segment_vectors = nearest_waypoints_next - nearest_waypoints
-        wp_segment_vector_norms = tf.norm(wp_segment_vectors, axis=2)
+        wp_segment_vector_norms = self.lib.norm(wp_segment_vectors, axis=2)
         
         # Get the vector from the next waypoint to the car position
-        vector_car_pos_to_nearest_wp = tf.subtract(car_positions,nearest_waypoints)
-        vector_car_pos_to_nearest_wp_norm = tf.norm(vector_car_pos_to_nearest_wp, axis=2)
-        vector_car_pos_to_nearest_wp_norm_square = tf.multiply(vector_car_pos_to_nearest_wp_norm, vector_car_pos_to_nearest_wp_norm)
+        vector_car_pos_to_nearest_wp = self.lib.subtract(car_positions,nearest_waypoints)
+        vector_car_pos_to_nearest_wp_norm = self.lib.norm(vector_car_pos_to_nearest_wp, axis=2)
+        vector_car_pos_to_nearest_wp_norm_square = self.lib.multiply(vector_car_pos_to_nearest_wp_norm, vector_car_pos_to_nearest_wp_norm)
         
         # Get projection of car_pos_to_nearest_wp on nearest wp_segment
-        car_pos_to_nearest_wp_dot_wp_segment =  self.lib.sum(vector_car_pos_to_nearest_wp * wp_segment_vectors, 2)
+        car_pos_to_nearest_wp_dot_wp_segment =  self.lib.sum(vector_car_pos_to_nearest_wp * wp_segment_vectors, axis=2)
         projection_car_pos_to_wp_on_wp_segment = car_pos_to_nearest_wp_dot_wp_segment / wp_segment_vector_norms
-        projection_square = tf.multiply(projection_car_pos_to_wp_on_wp_segment, projection_car_pos_to_wp_on_wp_segment)
+        projection_square = self.lib.multiply(projection_car_pos_to_wp_on_wp_segment, projection_car_pos_to_wp_on_wp_segment)
         
         # Pytagoras
         distance_to_segment_square = vector_car_pos_to_nearest_wp_norm_square - projection_square
@@ -317,7 +316,7 @@ class f1t_cost_function(cost_function_base):
     def get_velocity_difference_to_wp_cost(self, s, waypoints, nearest_waypoint_indices):
         
         # if (velocity_diff_to_waypoints_cost_weight == 0): # Don't calculate if cost is 0
-        #     return tf.zeros_like(s[:,:,0])
+        #     return self.lib.zeros_like(s[:,:,0])
 
         car_vel_x = s[:, :, LINEAR_VEL_X_IDX]
         velocity_difference_to_wp = self.get_velocity_difference_to_wp(car_vel_x, waypoints, nearest_waypoint_indices)
@@ -327,11 +326,11 @@ class f1t_cost_function(cost_function_base):
     
     def get_distance_to_wp_cost(self, s, waypoints, nearest_waypoint_indices):
         car_positions = s[:, :, POSE_X_IDX:POSE_Y_IDX + 1]  # TODO: Maybe better access separatelly X&Y and concat them afterwards.
-        nearest_waypoints = tf.gather(waypoints, nearest_waypoint_indices)
+        nearest_waypoints = self.lib.gather(waypoints, nearest_waypoint_indices, axis=0)
         waypoint_positions = nearest_waypoints[:,:, 1:3]
 
         wp_car_vector = car_positions - waypoint_positions
-        wp_car_vector_norms = tf.norm(wp_car_vector, axis=2)
+        wp_car_vector_norms = self.lib.norm(wp_car_vector, axis=2)
         return 1.0 * wp_car_vector_norms
         
     
@@ -340,39 +339,39 @@ class f1t_cost_function(cost_function_base):
         # Get nearest and the nearest_next waypoint for every position on the car's rollout
         speed_control = u[:, :, TRANSLATIONAL_CONTROL_IDX]
         velocity_difference_to_wp = self.get_velocity_difference_to_wp(speed_control, waypoints, nearest_waypoint_indices)
-        horizon = tf.cast(tf.shape(s)[1], dtype=tf.float32)
+        horizon = self.lib.cast(self.lib.shape(s)[1], self.lib.float32)
         velocity_difference_to_wp_normed = velocity_difference_to_wp / horizon
         return speed_control_diff_to_waypoints_cost_weight * velocity_difference_to_wp_normed
         
     def get_velocity_difference_to_wp(self, car_vel, waypoints, nearest_waypoint_indices):
 
-        nearest_waypoints = tf.gather(waypoints, nearest_waypoint_indices)
+        nearest_waypoints = self.lib.gather(waypoints, nearest_waypoint_indices, axis=0)
 
         nearest_waypoint_vel_x = waypoint_velocity_factor * nearest_waypoints[:,:,5]
 
         # nearest_waypoint_vel_x = self.lib.clip(nearest_waypoint_vel_x, 0.5, 17.5)
         
-        vel_difference = tf.abs(nearest_waypoint_vel_x - car_vel)
+        vel_difference = self.lib.abs(nearest_waypoint_vel_x - car_vel)
         return vel_difference
     
     def get_angle_difference_to_wp_cost(self, s, waypoints, nearest_waypoint_indices):
 
-        nearest_waypoints = tf.gather(waypoints, nearest_waypoint_indices)[:,:,3]
-        nearest_waypoint_psi_rad_sin = tf.sin(nearest_waypoints)
-        nearest_waypoint_psi_rad_cos = tf.cos(nearest_waypoints)
+        nearest_waypoints = self.lib.gather(waypoints, nearest_waypoint_indices, axis=0)[:,:,3]
+        nearest_waypoint_psi_rad_sin = self.lib.sin(nearest_waypoints)
+        nearest_waypoint_psi_rad_cos = self.lib.cos(nearest_waypoints)
         
         car_angle_sin = s[:, :, POSE_THETA_SIN_IDX]
         car_angle_cos = s[:, :, POSE_THETA_COS_IDX]
         
-        angle_difference_sin = tf.square(nearest_waypoint_psi_rad_sin - car_angle_sin)
-        angle_difference_cos= tf.square(nearest_waypoint_psi_rad_cos - car_angle_cos)
+        angle_difference_sin = self.lib.square(nearest_waypoint_psi_rad_sin - car_angle_sin)
+        angle_difference_cos= self.lib.square(nearest_waypoint_psi_rad_cos - car_angle_cos)
         return angle_difference_to_wp_cost_weight * (angle_difference_sin + angle_difference_cos)
         
     def normed_discount(self, array_to_discount, model_array, discount_factor):
         discount_vector = self.lib.ones_like(model_array) * discount_factor
-        discount_vector = tf.cast(discount_vector, dtype=tf.float32)
+        discount_vector = self.lib.cast(discount_vector, self.lib.float32)
         discount_vector = self.lib.cumprod(discount_vector, 0)
-        norm_factor = self.lib.sum(self.lib.ones_like(discount_vector), 0)/self.lib.sum(discount_vector, 0)
+        norm_factor = self.lib.sum(self.lib.ones_like(discount_vector), axis=0)/self.lib.sum(discount_vector, axis=0)
         discount_vector = norm_factor*discount_vector
         return array_to_discount*discount_vector
 
